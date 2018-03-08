52e580f5bedb62771e3f0c9d13e9bf35c11f792c
Richard.Dang <richard.dang@arcserve.com>
2018-03-08 14:54:31 +0800

    init v1 for eggs dropping puzzle

---
+++ Dynamic Programming/MediumLevel/Egg Dropping Puzze.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+ const int M = 50;
+
+int edp[M][M];
+
+
+int main(int argc, char* argv[])
+{
+    int t       = 0;
+    int eggs    = 0;
+    int floors  = 0;
+    
+    cin>>t;
+
+
+
+
+    return 0;
+}
