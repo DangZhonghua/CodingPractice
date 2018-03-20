e5d9e90fa589e0ce4bbc6a4d624367f76347428d
DangZhonghua <forseekme@163.com>
2018-03-20 21:50:27 +0800

    BCC home work

--- Graph/Medium/Biconnected Graph.cpp
+++ Graph/Medium/Biconnected Graph.cpp
@@ -26,3 +26,100 @@
 // 0
 
 // if there are no cut vertices in the graph, the Graph is the biconntected graph
+
+/*
+BiConnect graph at first, is connect graph. Bi-connect means there are different paths between
+any vertices u,v. If graph is connected and there is no cutvertices, then graph is bi-connected graph.
+
+Tarjan algorithm can detect BiConnect Component(BCC) based on DFS traversal. like his SCC algorithm,
+during DFS, v.order and v.lowlink
+*/
+
+
+#include<vector>
+#include<unordered_map>
+#include<iostream>
+using namespace std;
+
+struct dfsvertex
+{
+ bool m_bVisited;
+ int  m_nDepth;
+ int  m_nLowlink;
+ bool m_bCutVertex;
+ int  m_nParentVertex;
+ int  m_nChildCount;
+    dfsvertex()
+    {
+        m_bVisited      = false;
+        m_nDepth        = 0;
+        m_nLowlink      = 0;
+        m_bCutVertex    = false;
+        m_nParentVertex = -1;
+        m_nChildCount   = 0;
+    }
+};
+
+
+
+int bccDetect(vector< vector<int > >& G,unordered_map<int,dfsvertex>& vInfomap, int u, int Seq)
+{
+    ++Seq; //Record the DFS scan sequence.
+    vInfomap[u].m_nDepth = Seq;
+    vInfomap[u].m_nLowlink = Seq; //This may update during visit its descendents.
+    
+    
+    for(auto vIt = G[u].begin(); vIt != G[u].end(); ++vIt)
+    {
+        if(!vInfomap[*vIt].m_bVisited) //This is tree edge.
+        {
+            vInfomap[*vIt].m_nParentVertex = u;
+            vInfomap[u].m_nChildCount += 1;
+            bccDetect(G,vInfomap,*vIt, Seq);
+            if( vInfomap[*vIt].m_nLowlink > vInfomap[u].m_nDepth)
+            {
+                vInfomap[u].m_bCutVertex = true;
+            }
+            vInfomap[u].m_nLowlink  = vInfomap[u].m_nLowlink < vInfomap[*vIt].m_nLowlink? vInfomap[u].m_nLowlink : vInfomap[*vIt].m_nLowlink;
+        }        
+        else if( (*vIt) != vInfomap[u].m_nParentVertex)
+        {
+            vInfomap[u].m_nLowlink = vInfomap[u].m_nLowlink<vInfomap[u].m_nDepth?  vInfomap[u].m_nLowlink:vInfomap[u].m_nDepth; 
+        }
+    }
+
+
+    return 0;
+}
+
+
+int DetectBCG_TarjanAlgo(vector< vector<int > >& G, int V)
+{
+    int Seq = 0;
+    int nConnectedComponents = 0;
+    unordered_map<int, dfsvertex> vertexinfo;
+    dfsvertex dfsv;
+    for(int v = 0; v<V; ++v)
+    {
+        vertexinfo[v] = dfsv;
+    }
+
+    for(int v = 0; v<V; ++v)
+    {
+        if(!vertexinfo[v].m_bVisited) // Every vertex only need be scanned once.
+        {
+            bccDetect(G,vertexinfo,v,Seq);
+            ++nConnectedComponents;
+        }
+    }
+    
+
+    return 0;
+}
+
+
+
+int BiConnectedGraph(vector< vector<int > >& G, int V)
+{
+    return 0;
+}
