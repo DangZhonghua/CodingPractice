3ee7266104851377dec3563715d3f305261bd678
Richard.Dang <richard.dang@arcserve.com>
2018-03-20 17:13:07 +0800

    add biconnected graph init version

---
+++ Graph/Medium/Biconnected Graph.cpp
@@ -0,0 +1,28 @@
+//https://practice.geeksforgeeks.org/problems/biconnected-graph/0/?ref=self
+// Given a graph, check whether it is Biconnected or not. 
+
+// Input:
+// First line consists of T test cases. First line of every test case consists of 2 integers N, E,denoting the number of vertices and number of edges respectively. Second line of every test case consists of pair of E spaced integers, denoting the edges connecting each other.
+
+// Output:
+// Single line output, print 1 if graph is biconnected else 0.
+
+// Constraints:
+// 1<=T<=100
+// 1<=N,E<=100
+
+// Example:
+// Input:
+// 3
+// 2 1
+// 0 1
+// 5 6
+// 1 0 0 2 2 1 0 3 3 4 2 4
+// 3 2
+// 0 1 1 2
+// Output:
+// 1
+// 1
+// 0
+
+// if there are no cut vertices in the graph, the Graph is the biconntected graph
