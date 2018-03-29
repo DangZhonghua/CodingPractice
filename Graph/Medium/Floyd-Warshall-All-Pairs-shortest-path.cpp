522559342efca0a851b6c476a847171132812c28
Richard.Dang <richard.dang@arcserve.com>
2018-03-29 13:15:00 +0800

    Floyd warshall

--- Graph/Medium/Floyd-Warshall-All-Pairs-shortest-path.cpp
+++ Graph/Medium/Floyd-Warshall-All-Pairs-shortest-path.cpp
@@ -11,7 +11,7 @@ Output:
 For each test case output will be V*V space separated integers where the i-jth integer denote the shortest distance of ith vertex from jth vertex.
 
 Constraints:
-1<=T<=20 
+1<=T<=20
 1<=V<=20
 -1000<=graph[][]<=1000
 
@@ -24,8 +24,8 @@ Input
 0 1 43 1 0 6 43 6 0
 
 Output
-0 25 25 0 
-0 1 7 1 0 6 7 6 0 
+0 25 25 0
+0 1 7 1 0 6 7 6 0
 
 */
 
@@ -36,7 +36,7 @@ In the Floyd-Warshall algorithm, we use a different characterization of the stru
 in the matrix-multiplication-based all-pairs algorithms.
 The algorithm considers the "intermediate" vertices of a shortest path, where an intermediate vertex of a simple path p = 〈v1, v2,..., vl〉 is any vertex of p other than v1 or vl, that is, any vertex in the set {v2, v3,..., vl-1}.
 
-The Floyd-Warshall algorithm is based on the following observation. 
+The Floyd-Warshall algorithm is based on the following observation.
 Under our assumption that the vertices of G are V = {1, 2,..., n}, let us consider a subset {1, 2,..., k} of vertices for some k.
 For any pair of vertices i, j ∈ V, consider all paths from i to j whose intermediate vertices are all drawn from {1, 2,..., k}, and let p be a minimum-weight path from among them. (Path p is simple.)
 The Floyd-Warshall algorithm exploits a relationship between path p and shortest paths from i to j with all intermediate vertices in the set {1, 2,..., k - 1}. The relationship depends on whether or not k is an intermediate vertex of path p.
@@ -56,101 +56,94 @@ Similarly, p2 is a shortest path from vertex k to vertex j with all intermediate
 #include<climits>
 using namespace std;
 
-int Floyd_Warshall(vector< vector<int> >& D, int V)
+int Floyd_Warshall(vector< vector<long long> >& D, vector< vector<long long> >&  ND, int V)
 {
-    vector< vector<int> >  ND;
-    vector< vector<int> >* curMatrix;
-    vector< vector<int> >* nextMatrix;
-    
-    for(int u = 0; u<V; ++u)
-    {
-        ND.push_back(vector<int>());
-        for(int v = 0; v<V; ++v)
-        {
-            ND[u].push_back(INT_MAX);
-        }
-    }
+	vector< vector<long long> >* curMatrix;
+	vector< vector<long long> >* nextMatrix;
 
-    curMatrix   = &D;
-    nextMatrix  = &ND;  
+	curMatrix = &D;
+	nextMatrix = &ND;
+	bool bFirstTime = true;
+	for (int k = 0; k < V; ++k)
+	{
+		for (int u = 0; u < V; ++u)
+		{
+			for (int v = 0; v < V; ++v)
+			{
+				(*nextMatrix)[u][v] = (*curMatrix)[u][v];
+				if (INT_MAX != (*curMatrix)[u][k] && INT_MAX != (*curMatrix)[k][v])
+				{
+					if (((*curMatrix)[u][k] + (*curMatrix)[k][v]) < (*nextMatrix)[u][v])
+					{
+						(*nextMatrix)[u][v] = ((*curMatrix)[u][k] + (*curMatrix)[k][v]);
+					}
+				}
+			}
+		}
+		vector< vector<long long> >* t = curMatrix;
+		curMatrix = nextMatrix;
+		nextMatrix = t;
+	}
 
-    for(int k = 0; k<V; ++k)
-    {
-        for(int u = 0; u< V; ++u)
-        {
-            for(int v = 0; v<V; ++v)
-            {
-                (*nextMatrix)[u][v] = (*curMatrix)[u][v];
-                //if( INT_MAX != (*curMatrix)[u][k] && INT_MAX != (*curMatrix)[k][v])
-                {
-                     if( ((*curMatrix)[u][k] + (*curMatrix)[k][v]) < (*nextMatrix)[u][v] )
-                     {
-                         (*nextMatrix)[u][v] = ((*curMatrix)[u][k] + (*curMatrix)[k][v]);
-                     }
-                }
-                
-            }
-        }
-        vector< vector<int> >* t = curMatrix;
-        curMatrix =  nextMatrix;
-        nextMatrix = t;
-    }
 
-    if(V == 16)
-    {
-        D = (*nextMatrix);
-        
-    }
-    else
-    {
-        D = (*curMatrix);
-    }
-   
+	D = (*curMatrix);
 
-    return 0;
+	return 0;
 }
 
-int AllPairsShortestPath(vector< vector<int> >& D, int V)
+int AllPairsShortestPath(vector< vector<long long> >& D, int V)
 {
-    Floyd_Warshall(D,V);
+	vector< vector<long long> >  ND;
 
-    for(int u = 0; u<V;++u)
-    {
-        for(int v =0; v<V; ++v)
-        {
-            cout<<D[u][v]<<" ";
-        }
-    }
-    cout<<endl;
+	for (int u = 0; u < V; ++u)
+	{
+		ND.push_back(vector<long long>());
+		for (int v = 0; v < V; ++v)
+		{
+			ND[u].push_back(INT_MAX);
+		}
+	}
 
-    return 0;
+	Floyd_Warshall(D, ND, V);
+
+	for (int u = 0; u < V; ++u)
+	{
+		for (int v = 0; v < V; ++v)
+		{
+			cout << D[u][v] << " ";
+		}
+	}
+	cout << endl;
+	D.clear();
+	ND.clear();
+	return 0;
 }
 
 int main()
 {
-    
-    int t = 0;
-    int V = 0;
-    cin>>t;
-    while(t)
-    {
-        --t;
-        
-        cin>>V;
-        vector< vector<int> > D;
-        for(int u = 0; u<V; ++u)
-        {
-            D.push_back(vector<int>());
-            for(int v = 0; v<V; ++v)
-            {
-                int e;
-                cin>>e;
-                D[u].push_back(e);
-            }
-        }
-        AllPairsShortestPath(D,V);
 
-    }
+	int t = 0;
+	int V = 0;
+	cin >> t;
+	while (t)
+	{
+		--t;
 
-    return 0;
+		cin >> V;
+		vector< vector<long long> > D;
+		for (int u = 0; u < V; ++u)
+		{
+			D.push_back(vector<long long>());
+			for (int v = 0; v < V; ++v)
+			{
+				int e;
+				cin >> e;
+				D[u].push_back(e);
+			}
+		}
+		AllPairsShortestPath(D, V);
+
+	}
+
+	return 0;
 }
