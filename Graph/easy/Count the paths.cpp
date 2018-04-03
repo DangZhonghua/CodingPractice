431a7b3bac667d42bacc2df27a5245d670aa5f1a
Richard.Dang <richard.dang@arcserve.com>
2018-04-03 10:04:13 +0800

    solution of path count B/W s and d

--- Graph/easy/Count the paths.cpp
+++ Graph/easy/Count the paths.cpp
@@ -31,87 +31,119 @@ Output:
 #include<vector>
 using namespace std;
 
+#define WHITE   0
+#define GRAY    1
+#define BLACK   2
 
-
-int DFSVisit(int d, vector< vector<int> >& matrix, vector<int>& visited,int u, bool& bfind, int& nPath)
+int DFSVisit(int d, int p, int u, vector< vector<int> >& matrix, vector<int>& visited, vector<int>& path, vector<int>& find)
 {
-    visited[u] = 1;
-    if(u == d)
-    {
-        bfind = true;
-        nPath = 1;
-    }
-    
-    for(auto it = matrix[u].begin(); it != matrix[u].end(); ++it)
-    {
-        bool b = false;
-        int  nChildPath = 0;
-        if(!visited[*it])
-        {
-            DFSVisit(d,matrix,visited,b,nChildPath);
-        }
-    }
+	visited[u] = GRAY;
+	if (u == d)
+	{
+		find[u] = true;
+		path[u] = 1;
+	}
+	else
+	{
+		for (auto it = matrix[u].begin(); it != matrix[u].end(); ++it)
+		{
+			if ( WHITE == visited[*it] && u != *it)
+			{
+				DFSVisit(d, u, *it, matrix, visited, path, find);
+			}
+
+			if (BLACK == visited[*it]) //When the vertex has been completed, then add its path to current one.
+			{
+				path[u] += path[*it];
+			}
+
+			if (find[*it])
+			{
+				find[u] = true;
+			}
+		}
+	}
+
+	visited[u] = BLACK;
 
 
-
-
-    return 0;
+	return 0;
 }
 
 
 int CountPathsBWSourceAndDestination(int s, int d, vector< vector<int> >& matrix, int V)
 {
-    vector<int> visited;
+	vector<int> visited;
+	vector<int> path;
+	vector<int> find;
+	for (int i = 0; i < V; ++i)
+	{
+		visited.push_back(0);
+		path.push_back(0);
+		find.push_back(0);
+	}
 
-    for(int i = 0; i<V; ++i)
-    {
-        visited.push_back(0);
-    }
-    
-    bool bfind  = false;
-    int nPath   = 0;
+	bool bfind = false;
 
-    DFSVisit(d,matrix,visited,s,bfind,nPath);
+	DFSVisit(d, s, s, matrix, visited, path, find);
 
 
-    return 0;
+	if (path[s])
+	{
+		cout << path[s] << endl;
+	}
+	else
+	{
+		if (s == d)
+		{
+			cout << 1 << endl;
+		}
+		else
+		{
+			cout << 0 << endl;
+		}
+	}
+
+	return 0;
 }
 
 
 
 int main()
 {
-    int t = 0;
-    int s = 0;
-    int d = 0;
-    int v = 0;
-    int e = 0;
+	int t = 0;
+	int s = 0;
+	int d = 0;
+	int v = 0;
+	int e = 0;
 
-    vector< vector<int> > matrix;
+	vector< vector<int> > matrix;
 
-    cin>>t;
-    
-    while(t--)
-    {
-        cin>>v>>e;
-        
-        for(int i = 0; i<v; ++i)
-        {
-            matrix.push_back(vector<int>());
-        }
-        
-        for(int i = 0; i<e; ++i)
-        {
-            int e1, e2;
-            cin>>e1>>e2;
-            matrix[e1].push_back(e2);
-        }
+	cin >> t;
 
-        cin>>s>>d;
+	while (t)
+	{
+		--t;
+		cin >> v >> e;
 
-        CountPathsBWSourceAndDestination(s,d,matrix,v);
+		for (int i = 0; i < v; ++i)
+		{
+			matrix.push_back(vector<int>());
+		}
 
-    }
+		for (int i = 0; i < e; ++i)
+		{
+			int e1, e2;
+			cin >> e1 >> e2;
+			matrix[e1].push_back(e2);
+		}
+
+		cin >> s >> d;
+
+		CountPathsBWSourceAndDestination(s, d, matrix, v);
+		matrix.clear();
+
+	}
 
 
 
