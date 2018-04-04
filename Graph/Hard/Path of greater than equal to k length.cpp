b3f64876a448fb68116db0aaaa175787e4a3774e
DangZhonghua <forseekme@163.com>
2018-04-04 21:46:33 +0800

    slove path no less than K

--- Graph/Hard/Path of greater than equal to k length.cpp
+++ Graph/Hard/Path of greater than equal to k length.cpp
@@ -5,7 +5,9 @@ Source vertex should always be  0.
 
 Input:
 First Line contains an integer T denoting the number of test cases. Then T test cases follow.
-Each test case contains two lines. First Line contains three integers V, E and k representing vertices, edges of the graph and the required minimum length respectively. Second line contains 3 * E integers containing the information of all edges in the graph. Information of a single edge is a triplet in the following format: (Source Destination Distance). See example for more understanding.
+Each test case contains two lines. First Line contains three integers V, E and k
+representing vertices, edges of the graph and the required minimum length respectively.
+Second line contains 3 * E integers containing the information of all edges in the graph. Information of a single edge is a triplet in the following format: (Source Destination Distance). See example for more understanding.
 
 Output:
 For each test case print 1 if the path of atleast k distance exists, else print 0 in a new line.
@@ -35,3 +37,91 @@ Explanation:
 Test Case 2: There exists no path which has a distance of 8
 
 */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct vertex
+{
+    int v;
+    int weight;
+};
+
+int DFSpath(vector< vector<vertex> >& G, vector<int>& visited,int V, bool& bFind, int& d,int u,int K)
+{ 
+    visited[u] = 1;
+    if(d >= K)
+    {
+        bFind = true;
+    }
+
+    for(int i  = 0; i<G[u].size(); ++i)
+    {   
+        if(!visited[G[u][i].v])
+        {
+            d += G[u][i].weight;
+            DFSpath(G,visited,V,bFind,d,G[u][i].v,K);
+            d -= G[u][i].weight;                
+        }
+    }
+    visited[u] =  0;
+    
+    return 0;
+}
+
+
+int FindPathNoLessThanK( vector< vector<vertex> >& G, int V, int K)
+{
+    vector<int> visited;
+    
+    for(int i = 0; i<=V; ++i)
+    {
+        visited.push_back(0);
+    }
+
+    int d = 0;
+    int u = 0;
+    bool bFind = false;
+    DFSpath(G,visited, V, bFind,d,0,K);
+    
+    cout<<(bFind?1:0)<<std::endl;
+
+    return 0;
+}
+
+int main()
+{
+    int t = 0;
+    int V = 0;
+    int E = 0;
+    int K = 0;
+    vector< vector<vertex> > G;
+    int u,v,w;
+    
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>V>>E>>K;
+        for(int v =0; v<V; ++v)
+        {
+            G.push_back(vector<vertex>());
+        }
+        for(int e = 0; e<E; ++e)
+        {
+            cin>>u>>v>>w;
+            vertex  node;
+            node.v = v; 
+            node.weight = w;
+            G[u].push_back(node);
+            node.v = u;
+            G[v].push_back(node);
+        }
+        FindPathNoLessThanK(G, V, K);
+        G.clear();
+    }
+
+
+    return 0;
+}
---
+++ Tree/Medium/Preorder Traversal and BST.cpp
@@ -0,0 +1,36 @@
+/*
+https://practice.geeksforgeeks.org/problems/preorder-traversal-and-bst/0
+
+
+Given an array, write a program that prints 1 if given array can represent preorder traversal of a BST, else prints 0.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases.
+The first line of each test case is N,N is the size of array.
+The second line of each test case contains N input A[i].
+
+Output:
+Should print 1 if given array can represent preorder traversal of BST. Otherwise 0.
+
+Constraints:
+1 <=T<= 55
+1 <= N <= 1000
+1 <= arr[i] <= 1000
+
+Example:
+
+Input:
+3
+5
+40 30 35 80 100
+8
+40 30 32 35 80 90 100 120
+8
+7  9 6 1 4 2 3 40
+
+Output:
+1
+1
+0
+
+*/
