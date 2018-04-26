dc09297ce6ff0a150db13560ff7e7abcd1c8b243
Richard.Dang <richard.dang@arcserve.com>
2018-04-26 09:25:38 +0800

    subset with xor

---
+++ Dynamic Programming/MediumLevel/Subsets with XOR value.cpp
@@ -0,0 +1,34 @@
+/*
+
+https://practice.geeksforgeeks.org/problems/subsets-with-xor-value/0/?ref=self
+Given an array arr[] of N integers and a integer K, find the number of subsets of arr[] having XOR of elements as K.
+
+Input:
+
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case consists of two lines. The first line of each test case contains two space separated integers N and K as described in the problem statement. Second line of each test case contains N space separated integers denoting the array arr[].
+
+Output:
+Corresponding to each test case, print a single line containing an integer denoting the total number of subsets of arr[] having XOR of thier elements as K.
+
+Constraints:
+1<=T<=10
+1<=N<=20
+1<=K<=100
+0<=arr[i]<=100
+
+
+Example:
+Input:
+1
+4 6
+6 9 4 2
+
+Output:
+2
+
+Explanation:
+The subsets are {4,2} and {6}
+
+
+*/
+
