96e30b397e75d3322d93649d32d84228cc714b4b
DangZhonghua <forseekme@163.com>
2018-05-09 21:50:32 +0800

    bool string value

---
+++ String/Easy/Boolean String Value .cpp
@@ -0,0 +1,48 @@
+/*
+https://practice.geeksforgeeks.org/problems/boolean-string-value/0/?ref=self
+
+
+Boolean String Value 
+
+
+Given a string consisting of only 0, 1, A, B, C where
+A = AND
+B = OR
+C = XOR
+Calculate the value of the string assuming no order of precedence and evaluation is done from left to right.
+
+Input:
+
+The first line of input contains an integer T denoting the number of test cases.
+The next T lines contains T strings.
+
+Output:
+
+Print the value of the string assuming no order of precedence and evaluation is done from left to right
+
+Constraints:
+
+1 ≤ T ≤ 100
+1 ≤ length(string)<=1000
+
+
+Examples:
+Input : 
+1
+1A0B1
+
+Output : 1
+ 
+
+1 AND 0 OR 1 = 1
+
+Input : 
+2
+1C1B1B0A0
+1A0B1
+
+Output : 
+0
+1
+
+*/
