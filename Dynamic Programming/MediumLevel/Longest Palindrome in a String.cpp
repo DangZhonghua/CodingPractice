ed840b408ec8f48f70f4dfb6bac5f25bf65ea7b5
DangZhonghua <forseekme@163.com>
2018-05-15 11:20:48 +0800

    slove longest Palindrome

--- Dynamic Programming/MediumLevel/Longest Palindrome in a String.cpp
+++ Dynamic Programming/MediumLevel/Longest Palindrome in a String.cpp
@@ -46,3 +46,105 @@ aabbaa
 
 
 */
+
+/*
+Dynamic Programming: overlapped sub-problem and optimal sub-structure.
+
+                0  if 0 == lpstring[i+1, j-1] 
+lpstring[i,j] = 
+                0  if s[i] != s[j]
+
+                lpstring[i+1,j-1] +2  if s[i] == s[j] 
+ 
+
+*/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+
+int lpsSubstring(string& strtext)
+{
+    int maxlen = 1;
+    int s = 0;
+    int e = 0;
+
+    int N = strtext.size();
+    
+    vector<int> row(N,1);
+    vector< vector<int> > m(N, row);
+
+    
+    
+    for(int l = 2; l<= N; ++l)
+    {
+        for(int i = 0; i<N-l+1; ++i)
+        {
+            int j = i+l-1;
+            if(strtext[i] == strtext[j])
+            {
+                if(i+1 > j-1)
+                {
+                    m[i][j] = 2;
+                    if(m[i][j]>maxlen)
+                    {
+                        maxlen = m[i][j];
+                        s = i;
+                        e = j;
+                    }
+                }
+                else
+                {
+                    if(0 == m[i+1][j-1])
+                    {
+                        m[i][j] = 0;
+                    }
+                    else
+                    {
+                        m[i][j] = m[i+1][j-1]+2;
+                        if(m[i][j]>maxlen)
+                        {
+                            maxlen = m[i][j];
+                            s = i;
+                            e = j;
+                        }
+                        
+                    }
+                }
+            }
+            else
+            {
+                m[i][j] = 0;    
+            }
+        }
+    }
+
+    for(int i = s; i<=e; ++i)
+    {
+        cout<<strtext[i];
+    }
+    cout<<endl;
+
+    return 0;
+}
+
+int main()
+{
+    int t = 0;
+    string strtext;
+
+    cin>>t;
+
+    while(t>0)
+    {
+        --t;
+        cin>>strtext;
+        lpsSubstring(strtext);
+        strtext.clear();
+    }
+
+
+    return 0;
+}
