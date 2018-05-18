8ceaa6b5980512c7d27a0ca9acfb3b5185158380
DangZhonghua <forseekme@163.com>
2018-05-18 16:49:13 +0800

    add 1,3,4 combine solution

--- Dynamic Programming/easy/Count ways to express N as the sum of 1,3 and 4.cpp
+++ Dynamic Programming/easy/Count ways to express N as the sum of 1,3 and 4.cpp
@@ -38,9 +38,54 @@ Test Case 1:  For N = 4,     we can expres 4 in { 1+1+1+1} {1+3} {3+1} {4} So, t
 This is dynamic programming problem:  overlapped sub-problems and optimal sub-structure.
 This is a variable of complete knapsack problem.
 
-c[N] = C[N-1]*C[N-3]*C[N-4]
+c[N] = C[N-1]+C[N-3]+C[N-4]
 c[0] = 1;
 
 */
 
-int Count
+#include<iostream>
+using namespace std;
+
+
+int comp[] = {1,3,4};
+
+int CountCombineWays(int N)
+{
+    unsigned long long  c[10001] = {0};
+    c[0] = 1;
+    for(int i = 1; i<=N;++i)
+    {
+        for(int n = 0; n<3; ++n)
+        {
+            if(i-comp[n]>=0)
+            {
+                c[i] += c[i-comp[n]];
+            }
+        }
+        c[i] %=(1000000007);
+    }
+
+    cout<<c[N]%(1000000007)<<endl;
+
+
+    return 0;
+}
+
+
+
+int main()
+{
+    int t = 0;
+    int N = 0;
+    
+    cin>>t;
+    
+    while(t>0)
+    {
+        --t;
+        cin>>N;
+        CountCombineWays(N);
+    }
+
+    return 0;
+}
