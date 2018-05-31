90f76aa16c6bb05c882921afb0624476ed966831
DangZhonghua <forseekme@163.com>
2018-05-31 21:45:50 +0800

    h

--- Dynamic Programming/easy/Palindromic patitioning.cpp
+++ Dynamic Programming/easy/Palindromic patitioning.cpp
@@ -1,6 +1,6 @@
 /*
 https://practice.geeksforgeeks.org/problems/palindromic-patitioning/0
-
+https://www.geeksforgeeks.org/dynamic-programming-set-17-palindrome-partitioning/
 Palindromic patitioning 
 
 Given a string, a partitioning of the string is a palindrome partitioning 
@@ -43,36 +43,101 @@ Output:
 #include<iostream>
 #include<string>
 #include<vector>
+#include<climits>
 using namespace std;
 
-bool IsPalindromicString(const string& strtxt, int i, int j)
+bool IsPalindromicString(const string& strtxt, int i, int j,  vector< vector<int> >& psp)
 {
-    bool bPalindromic = true;
-     while(i<j)
-     {
-         if(strtxt[i] != strtxt[j])
-         {
-             bPalindromic = false;
-             break;
-         }
-         ++i;
-         --j;
-     }
+    bool bPalindromic = false;
+    if((j-i+1) == 2)
+    {
+        if(strtxt[i] == strtxt[j])
+        {
+            bPalindromic = true;
+        }
+    }
+    else
+    {
+        if( strtxt[i] == strtxt[j] && psp[i+1][j-1] == 0)
+        {
+            bPalindromic = true;
+        }
+    }
 
     return bPalindromic;
 }
 
 
 
+
 //Matrix chain multiply extension.
+//This is O(N^3) solution
 int PalindromicPartition(string& strtext)
 {
     int N = strtext.size();
     vector< vector<int> > psp(N+1,vector<int>(N+1,0));
 
-    for(int i = 0; i<N; ++i)
+
+    for(int l = 2; l<=N; ++l)
     {
-        psp[i][i] = 0;
+        for(int i = 0; i<=N-l; ++i)
+        {
+          int j = i+l-1;
+          psp[i][j] = INT_MAX;
+          for(int k = i; k<j; ++k)
+          {
+              if(IsPalindromicString(strtext, i,j,psp))
+              {
+                  psp[i][j] = 0;
+                  break;
+              }
+              else
+              {
+                  if(psp[i][j] > (psp[i][k] + psp[k+1][j] + 1) )
+                  {
+                    psp[i][j] = (psp[i][k] + psp[k+1][j] + 1);
+                  }         
+              }
+          }
+        }
+    }
+
+    cout<<psp[0][N-1]<<endl;
+
+    return 0;
+}
+
+
+// This is O(n^2) solution
+bool IsPalindromicString(const string& strtxt, int i, int j,  vector< vector<bool> >& psp)
+{
+    bool bPalindromic = false;
+    if((j-i+1) == 2)
+    {
+        if(strtxt[i] == strtxt[j])
+        {
+            bPalindromic = true;
+        }
+    }
+    else
+    {
+        if( strtxt[i] == strtxt[j] && psp[i+1][j-1])
+        {
+            bPalindromic = true;
+        }
+    }
+
+    return bPalindromic;
+}
+int OptPalindromicPartition(string& strtext)
+{
+    int N = strtext.size();
+    vector< vector<bool> > psp(N+1,vector<bool>(N+1,false));
+    vector<int> mincut(N+1,0);
+
+    for(int i = 0; i<N; ++i )
+    {
+        psp[i][i] = true;
     }
 
     for(int l = 2; l<=N; ++l)
@@ -80,15 +145,37 @@ int PalindromicPartition(string& strtext)
         for(int i = 0; i<=N-l; ++i)
         {
           int j = i+l-1;
-          for(int k = i; k<j; ++k)
-          {
-
-          }
+  
+            if(IsPalindromicString(strtext, i,j, psp))
+            {
+                psp[i][j] = true;             
+            } 
         }
     }
+    
+    for(int i = 0; i<N; ++i)
+    {
+        if(psp[0][i])
+        {
+            mincut[i] = 0;
+        }
+        else
+        {
+            mincut[i] = INT_MAX;
+            for(int j = 0; j<i; ++j)
+            {
+                if(psp[j+1][i] && (1+mincut[j] /* since j is less than i, mincut[j] has been calculated*/)< mincut[i])
+                {
+                    mincut[i] = (1+mincut[j]);
+                }
 
+            }
+        }
+    }
+    
+    cout<<mincut[N-1]<<endl;
 
-    return 0;
+ return 0;
 }
 
 int  main()
@@ -102,7 +189,7 @@ int  main()
     {
         --t;
         cin>>strtext;
-        PalindromicPartition(strtext);
+        OptPalindromicPartition(strtext);
     }
 
     return 0;
