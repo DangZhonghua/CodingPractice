bbb1a6425f478d1b62c3a5ff710ca3f31180888a
DangZhonghua <forseekme@163.com>
2018-06-10 15:16:30 +0800

    h

--- Dynamic Programming/MediumLevel/Pots of Gold Game.cpp
+++ Dynamic Programming/MediumLevel/Pots of Gold Game.cpp
@@ -5,7 +5,9 @@ https://practice.geeksforgeeks.org/problems/pots-of-gold-game/1/?ref=self
 Two player X and Y are playing a game in which there are pots of gold arranged in a  line, each containing some gold coins. They get alternating turns in which the player can pick a pot from one of the ends of the line. The winner is the player who has a higher number of coins at the end. The objective is to maximize the number of coins collected by X, assuming Y also plays optimally. You are required to complete the method maxCoins which returns an integer denoting the max coins X could get while playing the game. You may assume that X starts the game. 
 
 Input:
-The first line of input contains an integer T denoting the no of test cases. Then T test cases follow. Each test case contains an integer denoting the no of gold pots. Then in the next line are N space separated values denoting the values (A[]) of gold coins in each pot.
+The first line of input contains an integer T denoting the no of test cases. 
+Then T test cases follow. Each test case contains an integer denoting the no of gold pots. 
+Then in the next line are N space separated values denoting the values (A[]) of gold coins in each pot.
 
 Output:
 For each test case in a new line print the maximum amount of money X can collect.
