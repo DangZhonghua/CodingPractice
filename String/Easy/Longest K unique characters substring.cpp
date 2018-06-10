847dfeb0aad08560ee1d413026d5f888a148de02
DangZhonghua <forseekme@163.com>
2018-06-10 22:12:41 +0800

    h

--- String/Easy/Longest K unique characters substring.cpp
+++ String/Easy/Longest K unique characters substring.cpp
@@ -1,6 +1,9 @@
 /*
 Longest K unique characters substring
-Given a string you need to print the size of the longest possible substring that has exactly k unique characters. If there is no possible substring print -1.
+https://practice.geeksforgeeks.org/problems/longest-k-unique-characters-substring/0
+
+Given a string you need to print the size of the longest possible substring that has exactly k unique characters.
+If there is no possible substring print -1.
 Example
 For the string aabacbebebe and k = 3 the substring will be cbebebe with length 7.
 
@@ -28,3 +31,110 @@ Output:
 
 
 */
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int LongestKUniqueSubstring(const string& strtxt, int K)
+{
+	int N = strtxt.size();
+	int c[256] = { 0 };
+
+	int i = 0;
+	int j = 0;
+	int maxlen = 0;
+	int nunique = 0;
+	int s = 0;
+	int e = 0;
+
+	c[strtxt[0]] = 1;
+	nunique = 1;
+	maxlen = 0;
+
+	if (K == 1)
+	{
+		maxlen = 1;
+	}
+
+	j = 1;
+
+	while (j<N && i<j)
+	{
+		if (c[strtxt[j]] == 0)
+		{
+			++nunique;
+			c[strtxt[j]] = 1;
+			if (nunique == K)
+			{
+				if (maxlen<j - i)
+				{
+					maxlen = j - i+1;
+					s = i;
+					e = j;
+				}
+			}
+
+			if (nunique > K)
+			{
+				while (nunique > K && i<j)
+				{
+					c[strtxt[i]] -= 1;
+					if (c[strtxt[i]] == 0)
+					{
+						--nunique;
+					}
+					++i;
+				}
+			}
+		}
+		else
+		{
+			c[strtxt[j]] += 1;
+			if (nunique == K)
+			{
+				if (maxlen<j - i + 1)
+				{
+					maxlen = j - i + 1;
+					s = i;
+					e = j;
+				}
+			}
+		}
+		++j;
+	}
+
+	if (maxlen == 0)
+	{
+		cout << -1 << endl;
+	}
+	else
+	{
+		//string longest = strtxt.substr(s, e);
+		//cout << longest << endl;
+		cout << maxlen << endl;
+	}
+
+	return 0;
+}
+
+
+int main()
+{
+	int t = 0;
+	string strtxt;
+	int K = 0;
+
+	cin >> t;
+
+	while (t--)
+	{
+		cin >> strtxt;
+		cin >> K;
+		LongestKUniqueSubstring(strtxt, K);
+	}
+	
+
+	return 0;
+}
---
+++ String/Easy/Remove b  and ac  from a given string.cpp
@@ -0,0 +1,28 @@
+/*
+Remove “b” and “ac” from a given string
+https://practice.geeksforgeeks.org/problems/remove-b-and-ac-from-a-given-string/0
+Given a string, eliminate all “b” and “ac” in the string, replace them in-place and iterate over the string once.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows.
+Each test case contains a string of length N.
+
+Output:
+Print the resultant substring after removing 'b' and 'ac' from string.
+
+Constraints:
+1 ≤ T ≤ 200
+1 ≤ N ≤ 200
+
+Example:
+Input:
+2
+acbac
+aababc
+
+Output:
+
+aaac
+
+
+*/
