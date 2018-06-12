4638cc74e9134f8dbe2f0119f2733ab34eec7733
Richard.Dang <richard.dang@arcserve.com>
2018-06-12 13:43:02 +0800

    w

--- Dynamic Programming/MediumLevel/Pass the semester.cpp
+++ Dynamic Programming/MediumLevel/Pass the semester.cpp
@@ -49,7 +49,103 @@ Explaination :
 
 Marks needed to pass the subject  10 +10 + 3 = 23
 
-Time taken to achieve the 23 marks : 12 + 6 + 8 =36
-
+Time taken to achieve the 23 marks : 12 + 16 + 8 =36
 
 */
+
+/*
+
+0-1 knapsack problem. Time stand for capacity and weight. Mark is the value.
+
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int PassSemester(int N, int leftTime, int* w, int* v, int target)
+{
+	vector< vector<int> > f(N + 1, vector<int>(leftTime + 1, 0));
+	vector< vector<int> > s(N + 1, vector<int>(leftTime+1, 0));
+
+	for (int i = 1; i <= N; ++i)
+	{
+		for (int l = 1; l <= leftTime; ++l)
+		{
+			f[i][l] = f[i - 1][l];
+			s[i][l] = s[i - 1][l];
+			if (l >= w[i] && f[i - 1][l - w[i]] + v[i] > f[i - 1][l])
+			{
+				f[i][l] = f[i - 1][l - w[i]] + v[i];
+				s[i][l] = i;
+			}
+		}
+	}
+
+	int r = N; 
+	int c = leftTime;
+	vector<int> vSelect;
+	while (r > 0 && c > 0)
+	{
+		if (s[r][c] == r)
+		{
+			vSelect.push_back(r);
+			c = c - w[r];
+			r = r-1;
+			
+		}
+		else
+		{
+			r = r-1;
+		}
+	}
+	int TimeUsage = 0;
+	
+	for (int i = 0; i < vSelect.size(); ++i)
+	{
+		TimeUsage += w[vSelect[i]];
+	}
+	if (f[N][leftTime] >= target)
+	{
+		cout << "YES" << " "<<TimeUsage<<endl;
+	}
+	else
+	{
+		cout << "NO"<<endl;
+	}
+	return 0;
+}
+
+
+int main()
+{
+	int t = 0;
+	int N = 0;
+	int leftTime = 0;
+	int target = 0;
+	int top[600];
+	int m[600];
+
+	cin >> t;
+
+	while (t--)
+	{
+		cin >> N;
+		cin >> leftTime;
+		cin >> target;
+
+		int i = 1;
+
+		while (i <= N)
+		{
+			cin >> top[i];
+			cin >> m[i];
+			++i;
+		}
+		PassSemester(N, leftTime, top, m, target);
+	}
+
+
+	return 0;
+}
