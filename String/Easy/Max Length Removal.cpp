cd77dc99a5029c367b26ab26ec89ed97c0c571b6
DangZhonghua <forseekme@163.com>
2018-06-17 11:26:08 +0800

    h

--- String/Easy/Max Length Removal.cpp
+++ String/Easy/Max Length Removal.cpp
@@ -39,38 +39,110 @@ Input
 010010
 1011110000
 
+1
+01110010101010011001000111001
+
+1
+100111100111110011100001111101111110000
+
+
 Output
 3
 6
 */
 
+/*
+
+ use extra O(n) space to record the subtring range.
+
+*/
+
 #include<string>
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int maxLengthRemoval(char* sztext )
+int maxLengthRemoval(const string&  sztext)
 {
-    for(int i = 0; sztext[i] != '\0'; ++i)
-    {
+	//used to record the actual index of element in sztext even though removal.
+	vector<int> vkeep(sztext.size() + 1, 0); 
+	int maxlen = 0;
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sztext.size(); ++i)
+	{
+		if ('1' == sztext[i])
+		{
+			vkeep[j] = i;
+			++j;
+		}
+		else if ('0' == sztext[i]) //this is valuable tricky code piece.
+		{
+			//Detect the removable substring.
+			if (j < 2)
+			{
+				vkeep[j] = i;
+				++j;
+				continue;
+			}
+			int k = j - 1;
+			if ('1' == sztext[vkeep[k]])
+			{
+				vkeep[j] = i;
+				++j;
+				continue;
+			}
+			else if ('0' == sztext[vkeep[k]])
+			{
+				if ('1' == sztext[vkeep[k - 1]])
+				{
+					// Now, we check this removal substring whether contrenate the previous.
+					int s = k - 1;
+					if (k - 2 >= 0)
+					{
+						s = k - 2;
+						if (i - vkeep[s]>maxlen)
+						{
+							maxlen = i - vkeep[s];
+						}
+					}
+					else
+					{
+						if (i - vkeep[s] + 1>maxlen)
+						{
+							maxlen = i - vkeep[s] + 1;
+						}
+					}
+	
+					j = k - 1;
+				}
+				else
+				{
+					vkeep[j] = i;
+					++j;
+				}		
+			}
+		}
+	}
 
-    }
+	cout << maxlen << endl;
 
-
-    return 0;
+	return 0;
 }
 
 int main()
 {
-    int t = 0;
-    char sztext[1001];
-    
-    cin>>t;
-    
-    while(t--)
-    {
-        cin>>sztext; 
-    }
+	int t = 0;
+	string sztext;
+
+	cin >> t;
+
+	while (t--)
+	{
+		cin >> sztext;
+		maxLengthRemoval(sztext);
+	}
 
 
-    return 0;
+	return 0;
 }
