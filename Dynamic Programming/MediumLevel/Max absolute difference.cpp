a9f8be3c544b6d21e7692667ec573e8bad868635
Richard.Dang <richard.dang@arcserve.com>
2018-06-19 16:53:33 +0800

    w

--- Dynamic Programming/MediumLevel/Max absolute difference.cpp
+++ Dynamic Programming/MediumLevel/Max absolute difference.cpp
@@ -2,8 +2,10 @@
 
 Max absolute difference 
 https://practice.geeksforgeeks.org/problems/max-absolute-difference/0
+https://www.geeksforgeeks.org/maximum-absolute-difference-between-sum-of-two-contiguous-sub-arrays/
 
-Given an array of integers, find two non-overlapping contiguous sub-arrays such that the absolute difference 
+Given an array of integers, find two non-overlapping contiguous sub-arrays 
+such that the absolute difference 
 between the sum of two sub-arrays is maximum.
 
 For example,
@@ -40,5 +42,86 @@ Output:
 16
 12
 
+*/
+
+/*
+
+This is familiar with matrix chain multipy problem: the target two subarraies must continuous,
+
+[i...j] [j+1...k].
+
+For certain length subarray, we just search the partition point: j
 
 */
+
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+int CalcMaxAbsoluteDiff(int*a, int N)
+{
+    vector< vector<int> > sm(N, vector<int>(N,0));
+    int maxDiff = INT_MIN;
+
+    for(int i = 0; i<N; ++i)
+    {
+        sm[i][i] = a[i];
+    }
+
+    //Calc the subarray sum.    
+    for(int i = 0; i<N; ++i)
+    {
+        for(int j = i+1; j<N; ++j)
+        {
+            sm[i][j] = sm[i][j-1]+a[j];
+        }
+    }
+
+    // ll: left subarray length.
+    for(int l = 2; l<=N; ++l)
+    {
+        for(int i = 0; i<=N-l; ++i)
+        {
+            int j = i+l-1;
+            for(int k = i; k<j; ++k)
+            {
+                int diff = sm[i][k]>sm[k+1][j]? (sm[i][k]-sm[k+1][j]):(sm[k+1][j]-sm[i][k]);
+                 if(diff>maxDiff)
+                 {
+                     maxDiff = diff;
+                 }
+            }
+        }
+    }
+
+    cout<<maxDiff<<endl;
+
+    return 0;
+}
+
+
+
+
+int main()
+{
+    int t = 0;
+    int a[1000];
+    int N = 0;
+    
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>N;
+        int i = 0;
+        while(i<N)
+        {
+            cin>>a[i++];
+        }
+        CalcMaxAbsoluteDiff(a,N);
+    }
+
+
+    return 0;
+}
