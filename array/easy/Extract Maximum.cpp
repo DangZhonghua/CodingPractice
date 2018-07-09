b27fa646f4dacb1d43d2c2bf415c693df1063a84
DangZhonghua <forseekme@163.com>
2018-07-09 11:24:45 +0800

    h

--- array/easy/Extract Maximum.cpp
+++ array/easy/Extract Maximum.cpp
@@ -2,7 +2,8 @@
 Extract Maximum 
 https://practice.geeksforgeeks.org/problems/extract-maximum/0
 
-You have been given an alphanumeric string , extract maximum numeric value from that string. Alphabets will only be in lower case.
+You have been given an alphanumeric string , extract maximum numeric value from that string. 
+Alphabets will only be in lower case.
 
 Input:
 The first line contains a single integer T i.e. the number of test cases. The first and only line consists of a String S.
@@ -30,6 +31,87 @@ Output:
 Explanation:
 Test Case 1: The maximum number found in the string is "564".  
 
+*/
 
+/*
+use range or slide window to handle this.
 
 */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int ExtractMax(const string& strexp)
+{
+    int s = -1;
+    int e = 0;
+    int max = 0;
+    //[s,e-1] is the slide window
+    while(e<strexp.length())
+    {
+        if(strexp[e]>='a' && strexp[e]<='z')
+        {
+            if( -1 != s) // the window has been formed.
+            {          
+                int base = 0;
+                for(int i = s; i<e; ++i)
+                {
+                    base *=10;
+                    base +=(strexp[i]-'0');
+                }
+                if(max<base)
+                {
+                    max = base;
+                }
+              s = -1;
+            }
+        }
+        else
+        {
+            if(-1 == s)
+            {
+                s = e;
+            }
+        }
+
+        ++e;    
+    } 
+
+    if(-1 != s) //the last window
+    {
+        int base = 0;
+        for(int i = s; i<e; ++i)
+        {
+            base *=10;
+            base +=(strexp[i]-'0');
+        }
+        if(max<base)
+        {
+            max = base;
+        }
+    }
+
+    
+
+    cout<<max<<endl;
+
+    return 0;
+}
+
+int main()
+{
+    int t = 0;
+    string strexp;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>strexp;
+        ExtractMax(strexp);
+    }
+
+    
+    return 0;
+}
