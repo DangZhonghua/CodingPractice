b706ae32d6e08270795a8d31717b1c9b3c2f192c
Richard.Dang <richard.dang@arcserve.com>
2018-07-09 22:43:48 +0800

    w

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64     
-set compilerflags=/Od /Zi /EHsc
+set compilerflags=/Od /Zi /EHsc /MD
 set linkerflags=/OUT:kw.exe
-cl.exe %compilerflags% "Graph\Medium\Knight Walk.cpp" /link %linkerflags%
+cl.exe %compilerflags% "Dynamic Programming/easy/Longest Bitonic subsequence.cpp" /link %linkerflags%
