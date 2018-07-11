dfd0f7a959a872b4ae8afeb8a9bade42c3b756e0
Richard.Dang <richard.dang@arcserve.com>
2018-07-11 13:56:13 +0800

    w

---
+++ array/easy/Two numbers with sum closest to zero.cpp
@@ -0,0 +1,36 @@
+/*
+Two numbers with sum closest to zero 
+https://practice.geeksforgeeks.org/problems/two-numbers-with-sum-closest-to-zero/0/?ref=self
+
+Given an integer array, you need to find the two elements such that their sum is closest to zero.
+ 
+Input:
+The first line of input contains an integer T denoting the number of test cases.  
+The first line of each test case is N,the size of array
+Each test case consist of a N integers which are the array elements.
+ 
+Output:
+Print the two numbers in ascending order such that their sum is closest to zero.
+ 
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 1000
+-100007 ≤ a[i] ≤ 100007
+ 
+Example:
+Input
+3
+3
+-8 -66 -60  
+6
+-21 -67 -37 -18 4 -65  
+2
+-24 -73  
+ 
+Output
+-60 -8
+-18 4
+-73 -24
+
+
+*/
