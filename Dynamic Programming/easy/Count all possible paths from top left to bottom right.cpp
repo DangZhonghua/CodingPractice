7901e27348b723b11037f37762eaf9c105223ff3
Richard.Dang <richard.dang@arcserve.com>
2018-07-12 17:03:02 +0800

    w

---
+++ Dynamic Programming/easy/Count all possible paths from top left to bottom right.cpp
@@ -0,0 +1,26 @@
+/*
+Count all possible paths from top left to bottom right 
+https://practice.geeksforgeeks.org/problems/count-all-possible-paths-from-top-left-to-bottom-right/0/?ref=self
+
+The task is to count all the possible paths from top left to bottom right of a mXn matrix with the constraints that from each cell you can either move only to right or down.
+
+Input: 
+First line consists of T test cases. First line of every test case consists of N and M, denoting the number of rows and number of column respectively.
+
+Output: 
+Single line output i.e count of all the possible paths from top left to bottom right of a mXn matrix. Since output can be very large number use %10^9+7.
+
+Constraints:
+1<=T<=100
+1<=N<=100
+1<=M<=100
+
+Example:
+Input:
+1
+3 3
+Output:
+6
+
+
+*/
