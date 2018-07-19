525fd8aa4cec2058f385b42e12888c0f7fb1599a
DangZhonghua <forseekme@163.com>
2018-07-19 23:23:58 +0800

    h

--- Dynamic Programming/MediumLevel/Special Keyboard.cpp
+++ Dynamic Programming/MediumLevel/Special Keyboard.cpp
@@ -1,17 +1,17 @@
 /*
-Special Keyboard 
+Special Keyboard
 https://practice.geeksforgeeks.org/problems/special-keyboard/0/?ref=self
 
-Imagine you have a special keyboard with the following keys: 
+Imagine you have a special keyboard with the following keys:
 Key 1:  Prints 'A' on screen
 Key 2: (Ctrl-A): Select screen
 Key 3: (Ctrl-C): Copy selection to buffer
 Key 4: (Ctrl-V): Print buffer on screen appending it
-                 after what has already been printed. 
+after what has already been printed.
 
-If you can only press the keyboard for N times (with the above four keys), 
-write a program to produce maximum numbers of A's. 
-That is to say, the input parameter is N (No. of keys that you can press), 
+If you can only press the keyboard for N times (with the above four keys),
+write a program to produce maximum numbers of A's.
+That is to say, the input parameter is N (No. of keys that you can press),
 the output is M (No. of As that you can produce).
 
 Input:
@@ -43,13 +43,13 @@ Explanation:
 
 Input:  N = 3
 Output: 3
-We can at most get 3 A's on screen by pressing 
+We can at most get 3 A's on screen by pressing
 following key sequence.
 A, A, A
 
 Input:  N = 7
 Output: 9
-We can at most get 9 A's on screen by pressing 
+We can at most get 9 A's on screen by pressing
 following key sequence.
 A, A, A, Ctrl A, Ctrl C, Ctrl V, Ctrl V
 
@@ -57,15 +57,15 @@ A, A, A, Ctrl A, Ctrl C, Ctrl V, Ctrl V
 */
 
 /*
-                C[i-1] + 1  if presss A
+C[i-1] + 1  if presss A
 
-  C[i] =  Max   C[i-1]      if press Ctrl-A
+C[i] =  Max   C[i-1]      if press Ctrl-A
 
-                C[i-1]      if press Ctrl-C
-            
-                C[i-3]*2    if press Ctrl-V
+C[i-1]      if press Ctrl-C
 
-                C[i-1]+ C[i-5] for press ctrl-V continously.
+C[i-3]*2    if press Ctrl-V
+
+C[i-1]+ CountByCopied[i-1]
 
 
 */
@@ -83,20 +83,36 @@ int maximumSeq(int N)
 		return 0;
 	}
 
-	vector<int> SK(N + 1, 0);
+	vector<long long> SK(N + 1, 0);
+	vector<long long> BC(N + 1, 0);
 	SK[1] = 1;
 
-	for (int i = 2; i <= N; ++i)
+	for (int i = 1; i < N; ++i)
 	{
-		SK[i] = SK[i - 1] + 1;
-		if (i>3 && SK[i] < SK[i - 3] * 2)
+		if (SK[i + 1] < SK[i] + 1)
 		{
-			SK[i] = SK[i - 3] * 2;
+			SK[i+1] = SK[i] + 1;
+			BC[i + 1] = BC[i];
 		}
-		if (i>5 && SK[i] < SK[i - 1] + SK[i - 5])
+		
+		if (i + 3 <= N)
 		{
-			SK[i] = SK[i - 1] + SK[i - 5];
+			if (SK[i + 3] <= SK[i] * 2)
+			{
+				SK[i + 3] = SK[i] * 2;
+				BC[i + 3] = SK[i];
+			}
 		}
+		if (i + BC[i] <= N)
+		{
+			if (SK[i + BC[i]] <= SK[i] + BC[i])
+			{
+				SK[i + BC[i]] = SK[i] + BC[i];
+				BC[i + BC[i]] = BC[i];
+			}
+		}
+
+
 	}
 
 	cout << SK[N] << endl;
