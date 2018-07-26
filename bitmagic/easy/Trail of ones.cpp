9bd49e1fd88e7ee61a9632ad760f320e5cc20cef
DangZhonghua <forseekme@163.com>
2018-07-26 22:55:22 +0800

    h

--- bitmagic/easy/Trail of ones.cpp
+++ bitmagic/easy/Trail of ones.cpp
@@ -43,3 +43,63 @@ Time: O(N)
 
 
 */
+
+/*
+
+To[N][1]    = TO[N-1][0]                        //end with only one 1
+TO[N][0]    = TO[N-1][1]+TO[N-1][0]+TO[N-1][2]  //end with 0
+TO[N][2]    = TO[N-1][2]+TO[N-1][1]            //end with at least two 1s
+
+For Input:
+2
+14
+25
+Output of Online Judge is:
+15397
+33358014
+
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int TrailOnes(int N)
+{
+    vector<vector<long long> > TO(N+1,vector<long long>(3,0));
+    TO[1][0] = 1;
+    TO[1][1] = 1;
+    TO[1][2] = 0;
+    long long count = 0;
+
+    for(int i = 2; i<=N; ++i)
+    {
+        TO[i][0] = TO[i-1][0] + TO[i-1][1] + TO[i-1][2];
+        TO[i][1] = TO[i-1][0];
+        TO[i][2] = TO[i-1][1] + TO[i-1][2];
+        count += TO[i][2];
+    }
+    
+    cout<<count<<endl;
+
+    return 0;
+}
+
+
+
+int main()
+{
+    int t = 0;
+    int N = 0;
+    
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>N;
+        TrailOnes(N);
+    }
+
+
+    return 0;
+}
