bb4a474e8bae3d5f27a251db4a7043f44c60cd6a
Richard.Dang <richard.dang@arcserve.com>
2018-07-26 13:34:14 +0800

    w

--- Dynamic Programming/MediumLevel/Min Coin.cpp
+++ Dynamic Programming/MediumLevel/Min Coin.cpp
@@ -48,6 +48,72 @@ Output :
 
 /*
 
-This is 0-1 knapsack problem.
+This is complete knapsack problem.
+1
+32 22417
+765 992 1 521 220 380 729 969 184 887 104 641 909 378 724 582 387 583 241 294 159 198 653 369 418 692 36 901 516 623 703 971
+
+
+mc[amount] = min{mc[amount-coin[i]] + 1}  if mc[amount-coin[i]] exist for i = 0...N
 
 */
+
+#include<iostream>
+#include<vector>
+#include<climits>
+#include <algorithm>
+using namespace std;
+
+int minCoin(int* coin, int N, int amount)
+{
+	//vector< vector<int> > mc(amount + 1, vector<int>(N + 1, INT_MAX));
+	vector< int >  mc(amount+1, INT_MAX);
+	mc[0] = 0;
+	
+	for (int capacity = 1; capacity <= amount; ++capacity)
+	{
+		for (int i = 0; i < N; ++i)
+		{
+			if (capacity >= coin[i] && INT_MAX != mc[capacity - coin[i]])
+			{
+				if (mc[capacity - coin[i]] + 1 < mc[capacity])
+				{
+					mc[capacity] = mc[capacity-coin[i]] +1;
+				}
+			}
+		}
+	}
+
+	if (INT_MAX == mc[amount])
+	{
+		cout<<-1<<endl;
+	}
+	else
+	{
+		cout << mc[amount]<<endl;
+	}
+
+	return 0;
+}
+
+int main()
+{
+	int t = 0;
+	int a[101];
+	int N = 0;
+	int amount = 0;
+
+	cin >> t;
+	while (t--)
+	{
+		cin >> N >> amount;
+		int i = 0;
+		while (i < N)
+		{
+			cin >> a[i++];
+		}
+		minCoin(a, N, amount);
+	}
+
+	return 0;
+}
