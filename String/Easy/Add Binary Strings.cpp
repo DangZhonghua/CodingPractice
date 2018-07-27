9220b22a7982bc0772bd4bb259245d971efb9f26
Richard.Dang <richard.dang@arcserve.com>
2018-07-27 16:25:55 +0800

    w

--- String/Easy/Add Binary Strings.cpp
+++ String/Easy/Add Binary Strings.cpp
@@ -29,5 +29,88 @@ Output:
 10100
 11
 
-
 */
+
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+int addBinaryString(const string& x, const string& y)
+{
+	stack<char> sr;
+	int  char2Int[2] = { 0, 1 };
+	char Int2char[2] = { '0', '1' };
+
+	int c = 0;
+
+	int i = x.length() - 1;
+	int j = y.length() - 1;
+
+	while (i >= 0 && j >= 0)
+	{
+		int s = c + char2Int[x[i] - '0'] + char2Int[y[j] - '0'];
+		c = 0;
+		if (s > 1)
+		{
+			c = s / 2;
+		}
+		sr.push(Int2char[s % 2]);
+		--i;
+		--j;
+	}
+
+	while (i >= 0)
+	{
+		int s = c + char2Int[x[i] - '0'];
+		c = 0;
+		if (s > 1)
+		{
+			c = s / 2;
+		}
+		sr.push(Int2char[s % 2]);
+		--i;
+	}
+	while (j >= 0)
+	{
+		int s = c + char2Int[y[j] - '0'];
+		c= 0;
+		if (s > 1)
+		{
+			c = s / 2;
+		}
+		sr.push(Int2char[s % 2]);
+		--j;
+	}
+	if (c)
+	{
+		sr.push(Int2char[c]);
+	}
+	
+	string strsum;
+
+	while (!sr.empty())
+	{
+		strsum += sr.top();
+		sr.pop();
+	}
+
+	cout << strsum << endl;
+
+	return 0;
+}
+
+
+int main()
+{
+	int t = 0;
+	string x, y;
+
+	cin >> t;
+	while (t--)
+	{
+		cin >> x >> y;
+		addBinaryString(x, y);
+	}
+	return 0;
+}
