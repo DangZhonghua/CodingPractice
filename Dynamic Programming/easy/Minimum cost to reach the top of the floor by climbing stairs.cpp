1578320012488771130dbf7db5146642a0aed851
DangZhonghua <forseekme@163.com>
2018-08-21 21:07:16 +0800

    h

--- Dynamic Programming/easy/Minimum cost to reach the top of the floor by climbing stairs.cpp
+++ Dynamic Programming/easy/Minimum cost to reach the top of the floor by climbing stairs.cpp
@@ -16,5 +16,49 @@ Input: a[] = {2, 5, 3, 1, 7, 3, 4}
 Output: 9 
 2->3->1->3
 
+*/
+
+/*
+
+cost[i] = min{ cost[i-2] + a[i-2, cost[i-1] + a[i-1] }
 
 */
+
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+int minimumCost2Top(vector<int>& a)
+{
+    vector<int> vc(a.size(),INT_MAX);
+    vc[0] = 0;
+    vc[1] = 0;
+    
+    for(int i = 2; i<a.size(); ++i)
+    {
+        if(vc[i]>vc[i-2]+a[i-2])
+        {
+            vc[i] = vc[i-2] + a[i-2];
+        }
+        if(vc[i]> vc[i-1] + a[i-1])
+        {
+            vc[i] = vc[i-1] + a[i-1];
+        }
+    }
+    
+    cout<<vc[a.size()-1]<<endl;
+    
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<int> a{ 16, 19, 10, 12, 18 };
+    vector<int> b{2, 5, 3, 1, 7, 3, 4 };
+
+    minimumCost2Top(a);
+    minimumCost2Top(b);
+
+    return 0;
+}
