42b6470922110a7c3cd5716bac43e92a8edb104b
Richard.Dang <richard.dang@arcserve.com>
2018-10-19 10:34:43 +0800

    w

--- bitmagic/easy/Rightmost different bit.cpp
+++ bitmagic/easy/Rightmost different bit.cpp
@@ -2,13 +2,16 @@
 Rightmost different bit
 https://practice.geeksforgeeks.org/problems/rightmost-different-bit/0/?ref=self
 
-Given two numbers M and N. Write a program to find the position of rightmost different bit in binary representation of numbers.
+Given two numbers M and N. Write a program to find the position of rightmost different bit 
+in binary representation of numbers.
 
 Input:
-First line of input contains a single integer T which denotes the number of test cases. T test cases follows. First line of each test case contains two space separated integers M and N.
+First line of input contains a single integer T which denotes the number of test cases. 
+T test cases follows. First line of each test case contains two space separated integers M and N.
 
 Output:
-For each test case print the position of rightmost different bit in binary representation of numbers. If both M and N are same then print -1 in this case.
+For each test case print the position of rightmost different bit in binary representation of numbers. 
+If both M and N are same then print -1 in this case.
 
 Constraints:
 1<=T<=100
@@ -25,3 +28,41 @@ Output:
 5
 
 */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t = 0;
+    cin>>t;
+    
+    while(t--)
+    {
+        int M = 0;
+        int N = 0;
+        cin>>M>>N;
+        
+        int res = M^N;
+        if(0 == res)
+        {
+            cout<<-1<<endl;
+        }
+        else
+        {
+            int num = 1;
+            unsigned int d = 1;
+            while(1)
+            {
+                if(d&res)
+                {
+                    break;
+                }
+                ++num;
+                d <<=1;
+            }
+            cout<<num<<endl;
+        }
+    }
+    return 0;    
+}
