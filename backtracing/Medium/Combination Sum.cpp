9afcb63d659ba763b0dbaef0ca1814415381fd14
DangZhonghua <forseekme@163.com>
2018-10-30 21:21:17 +0800

    h

--- backtracing/Medium/Combination Sum.cpp
+++ backtracing/Medium/Combination Sum.cpp
@@ -2,7 +2,7 @@
 Combination Sum
 https://practice.geeksforgeeks.org/problems/combination-sum/0
 
-Given an array of integers A and a sum B, find all unique combinations in A where the sum is equal to B. 
+Given an array of integers A and a sum B, find all unique combinations in A where the sum is equal to B.
 The same repeated number may be chosen from A unlimited number of times.
 Note:
 All numbers will be positive integers.
@@ -25,7 +25,7 @@ Every test case has three lines.
 First line is N, size of array. 1<=N<=12
 Second line contains N space seperated integers(x). 1<=x<=9.
 Third line is the sum B. 1<=B<=30.
- 
+
 Output:
 One line per test case, every subset enclosed in () and in every set intergers should be space seperated.(See example)
 
@@ -51,6 +51,11 @@ Empty
 
 /*
 this can be done by using dynamic programming or backtracking technique.
+
+
+
+
+
 */
 
 #include<iostream>
@@ -61,47 +66,100 @@ using namespace std;
 
 void OutputCombination(vector<int>& vc)
 {
-    cout<<"(";
-    for(auto a:vc)
-    {
-        cout<<a<<" "; 
-    }
-    cout<<")";
+    int i = 0;
+	cout << "(";
+	for(i = 0; i<vc.size()-1; ++i)
+	{
+	    cout<<vc[i]<<" ";
+	}
+	cout <<vc[i]<<")";
 }
 
-bool btCombinationSum( vector<int>& a, vector<int>& vc, int sum, int index,bool& bFind)
+void btCombinationSum(vector<int>& a, vector<int>& vc, int index, int sum, bool& bFind)
 {
-    if(a.size()>index && sum>=a[index])
-    {
-        sum -= a[index];
-        vc.push_back(a[index]);
-        if(0 == sum)
-        {
-            bFind = true;
-            OutputCombination(vc);
-        }
-        btCombinationSum(a,vc,sum,index, bFind);
-    }
+
+	for (int i = index; i<a.size(); ++i)
+	{
+		if (sum >= a[i])
+		{
+			sum -= a[i];
+			vc.push_back(a[i]);
+			if (0 == sum)
+			{
+				bFind = true;
+				OutputCombination(vc);
+			}
+			else
+			{
+				btCombinationSum(a, vc, i, sum,bFind);
+			}
+			vc.pop_back();
+			sum += a[i];
+		}
+		else
+		{
+			break;
+		}
+	}
 
 }
 
 
 int CombinationSum(vector<int>&a, int sum)
 {
-    vector<int> vc;
-    bool bFind = false;
-    std::sort(a.begin(), a.end());
-    btCombinationSum(a,vc,sum,0,bFind);
-    if(!bFind)
-    {
-        cout<<"Empty"<<endl;
-    }
-    
-    return 0;
+	vector<int> vc;
+	bool bFind = false;
+	std::sort(a.begin(), a.end());
+	
+	vector<int> b;
+	b.push_back(a[0]);
+	int j = 0;
+	for(int i = 1; i<a.size(); ++i)
+	{
+	    if(a[i] == b[j])
+	    {
+	        continue;
+	    }
+	    else
+	    {
+	        b.push_back(a[i]);
+	        ++j;
+	    }
+	}
+	
+	btCombinationSum(b, vc, 0,sum, bFind);
+	if (!bFind)
+	{
+		cout << "Empty" << endl;
+	}
+	else
+	{
+		cout << endl;
+	}
+	return 0;
 }
 
 
 int main()
 {
+	int t = 0;
+	cin >> t;
 
+	while (t--)
+	{
+		int N = 0;
+		int Sum = 0;
+		cin >> N;
+		vector<int> a(N, 0);
+		int i = 0;
+		while (i < N)
+		{
+			cin >> a[i];
+			++i;
+		}
+		cin >> Sum;
+		CombinationSum(a, Sum);
+	}
+
+	return 0;
 }
