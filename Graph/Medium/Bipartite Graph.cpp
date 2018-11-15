30e7f90ac6846b50fe097792b9cb0a2f91c06aa2
Richard.Dang <richard.dang@arcserve.com>
2018-11-15 17:13:41 +0800

    w

--- Graph/Medium/Bipartite Graph.cpp
+++ Graph/Medium/Bipartite Graph.cpp
@@ -7,8 +7,8 @@ Given an adjacency matrix representation of a graph g having 0 based index.
 Your task is to complete the function is Bipartite which returns true if the graph is a bipartite graph else returns false.
 
 Input:
-The first line of input contains an integer T denoting the no of test cases. Then T test cases follow. 
-Each test case contains an integer v denoting the no of vertices of the graph then in the next line are v*v 
+The first line of input contains an integer T denoting the no of test cases. Then T test cases follow.
+Each test case contains an integer v denoting the no of vertices of the graph then in the next line are v*v
 space separated values of the adjacency matrix representation of the graph g.
 
 Output:
@@ -46,7 +46,7 @@ use BFS to two colored vertices of graph
 
 */
 
-#define MAX (20)
+#define MAX (4)
 
 #include<queue>
 #include<iostream>
@@ -54,19 +54,87 @@ use BFS to two colored vertices of graph
 using namespace std;
 
 
-bool isBipartite(int G[][MAX],int V)
+bool JudgeBiPartite(int G[][MAX], int V, int startVertex,unordered_map<int, int>& vertex2ColorMap )
 {
-     //Your code here
-     unordered_map<int,int> vertex2ColorMap;
-     queue<int> levelQ;
-     int nextColor = 2;
-     //From the vertex 0 to BFS the Graph.
-     vertex2ColorMap[0] = 1;
-     levelQ.push(0);
+	queue<int> levelQ;
+	bool  bBipartite = true;
+	int nextColor = 2;
+	int CurColor = 1;
+	int CurlevelCount = 1;
+	int NextLevelCount = 0;
+	//From the vertex 0 to BFS the Graph.
+	vertex2ColorMap[0] = CurColor;
+	levelQ.push(0);
+	while (CurlevelCount && bBipartite)
+	{
+		while (CurlevelCount && bBipartite)
+		{
+			int vertex = levelQ.front();
+			levelQ.pop();
+			--CurlevelCount;
+			for (int i = 0; i < V; ++i)
+			{
+				if (G[vertex][i])
+				{
+					if (vertex2ColorMap.find(i) == vertex2ColorMap.end())
+					{
+						levelQ.push(i);
+						vertex2ColorMap[i] = nextColor;
+						++NextLevelCount;
+					}
+					else if (vertex2ColorMap[i] == vertex2ColorMap[vertex])
+					{
+						bBipartite = false;
+						break;
+					}
+				}
+			}
+		}
+		if (!bBipartite)
+		{
+			break;
+		}
+		CurlevelCount = NextLevelCount;
+		int color = nextColor;
+		nextColor = CurColor;
+		CurColor = color;
+		NextLevelCount = 0;
+	}
 
-     
-     
+	return bBipartite;
+}
+
+bool isBipartite(int G[][MAX], int V)
+{
+	//Your code here
+    bool bBipartite = true;
+    unordered_map<int, int> vertex2ColorMap;
+    for(int i = 0; i<V; ++i)
+    {
+        if( vertex2ColorMap.find(i) == vertex2ColorMap.end() )
+        {
+            bBipartite &= JudgeBiPartite(G,V,i,vertex2ColorMap);
+        }
+        if(!bBipartite)
+        {
+            break;
+        }
+    }
+    return bBipartite;    
+}
 
 
+int main()
+{
+	int G[][4] = 
+	{
+		{ 0, 1, 0, 1 },
+		{ 1, 0, 1, 0 },
+		{ 0, 1, 0, 1 },
+		{ 1, 0, 1, 0 }
+	};
 
+	isBipartite(G,4);
+
+	return 0;
 }
