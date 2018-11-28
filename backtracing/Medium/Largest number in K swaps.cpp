fd4d06fa032ba9bdb96771d72042240c55d17e69
Richard.Dang <richard.dang@arcserve.com>
2018-11-28 17:16:28 +0800

    w

--- backtracing/Medium/Largest number in K swaps.cpp
+++ backtracing/Medium/Largest number in K swaps.cpp
@@ -1,6 +1,8 @@
 /*
 Largest number in K swaps 
 https://practice.geeksforgeeks.org/problems/largest-number-in-k-swaps/0
+https://www.geeksforgeeks.org/find-maximum-number-possible-by-doing-at-most-k-swaps/
+
 
 Given a number K and string S of digits denoting a positive integer, 
 build the largest number possible by performing swap operations on the digits of S atmost K times.
@@ -37,3 +39,23 @@ Output
 4301
 
 */
+#include<string>
+#include<iostream>
+using namespace std;
+
+
+void btSwap(string& x, int K, string& strMax)
+{
+    
+}
+
+int MaximumValueBySwap(string& x, int K)
+{
+    int ret = 0;
+    string strMax = 0;
+    
+    btSwap(x,K, strMax);
+
+
+    return ret;
+}
