6700ca87424806d88efefce6c9c406c0678b2c1a
Richard.Dang <richard.dang@arcserve.com>
2018-11-28 21:59:31 +0800

    w

---
+++ Dynamic Programming/easy/Max Sum without Adjacents.cpp
@@ -0,0 +1,33 @@
+/*
+Max Sum without Adjacents 
+https://practice.geeksforgeeks.org/problems/max-sum-without-adjacents/0
+https://www.geeksforgeeks.org/find-maximum-possible-stolen-value-houses/
+Given an array A of positive integers. Find the maximum sum of a subsequence such that no two numbers in the sequence should be adjacent in the array.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. The first line of each test case is N, size of array. The second line of each test case contains N elements.
+
+Output:
+Print the maximum sum of a subsequence.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 106
+1 ≤ Ci ≤ 107
+
+Example:
+Input:
+2
+6
+5 5 10 100 10 5
+4
+3 2 7 10
+
+Output:
+110
+13
+
+Explanation:
+Testcase 2 : 3 and 10 forms a non-continuous subsequence with maximum sum.
+
+*/
