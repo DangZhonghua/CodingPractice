abe845321a0db1c30c2ef1d8abda9b2cbf244d26
Richard.Dang <richard.dang@arcserve.com>
2018-11-28 21:54:07 +0800

    w

--- backtracing/Medium/Largest number in K swaps.cpp
+++ backtracing/Medium/Largest number in K swaps.cpp
@@ -1,14 +1,14 @@
 /*
-Largest number in K swaps 
+Largest number in K swaps
 https://practice.geeksforgeeks.org/problems/largest-number-in-k-swaps/0
 https://www.geeksforgeeks.org/find-maximum-number-possible-by-doing-at-most-k-swaps/
 
 
-Given a number K and string S of digits denoting a positive integer, 
+Given a number K and string S of digits denoting a positive integer,
 build the largest number possible by performing swap operations on the digits of S atmost K times.
 
 Input
-The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. 
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow.
 The first line of each test case contains a positive integer K.
 The second line of each test case contains a string of digits denoting a positive integer.
 
@@ -22,7 +22,7 @@ Constraints
 0 <   S <= 30
 0 <   K  <= 10
 
-Examples 
+Examples
 
 Input
 3
@@ -43,19 +43,63 @@ Output
 #include<iostream>
 using namespace std;
 
-
-void btSwap(string& x, int K, string& strMax)
+void swapChar(char& x, char& y)
 {
-    
+	char c = x;
+	x = y;
+	y = c;
+}
+
+void btSwap(string& x, int K, int start, string& strMax)
+{
+	if (0 == K)
+	{
+		return;
+	}
+
+	int len = x.length();
+	for (int i = start; i < len; ++i)
+	{
+		for (int j = i + 1; j < len; ++j)
+		{
+			if (x[i] < x[j]) // this is the possibility of making "number" larger.
+			{
+				swapChar(x[i], x[j]);
+				if (x.compare(strMax) > 0)
+				{
+					strMax = x;
+				}
+				btSwap(x, K - 1, start + 1, strMax);
+				swapChar(x[i], x[j]); // this is backtrace
+			}
+		}
+	}
+
 }
 
 int MaximumValueBySwap(string& x, int K)
 {
-    int ret = 0;
-    string strMax = 0;
-    
-    btSwap(x,K, strMax);
+	int ret = 0;
+	string strMax;
+	strMax = x; //for no swap occuring.
+	btSwap(x, K, 0, strMax);
+	cout << strMax << endl;
 
+	return ret;
+}
 
-    return ret;
+int main(int argc, char const *argv[])
+{
+	int t = 0;
+	cin >> t;
+
+	while (t--)
+	{
+		int K = 0;
+		cin >> K;
+		string x;
+		cin >> x;
+		MaximumValueBySwap(x, K);
+	}
+	return 0;
 }
