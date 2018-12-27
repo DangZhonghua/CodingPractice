773b2f2c60d886ac5d114dfe5a0de21fb2b62027
Richard.Dang <richard.dang@arcserve.com>
2018-12-27 14:00:13 +0800

    w

--- Dynamic Programming/easy/Minimum steps to delete a string.cpp
+++ Dynamic Programming/easy/Minimum steps to delete a string.cpp
@@ -1,15 +1,15 @@
 /*
-Minimum steps to delete a string 
+Minimum steps to delete a string
 https://practice.geeksforgeeks.org/problems/minimum-steps-to-delete-a-string/0/?ref=self
 https://www.geeksforgeeks.org/minimum-steps-to-delete-a-string-after-repeated-deletion-of-palindrome-substrings/
 
-Given a string containing characters as integers only. We need to delete all character of this string 
-in a minimum number of steps where in one step we can delete the substring which is a palindrome. 
+Given a string containing characters as integers only. We need to delete all character of this string
+in a minimum number of steps where in one step we can delete the substring which is a palindrome.
 After deleting a substring remaining parts are concatenated.
 
 
 Input:
-The first line of input contains an integer T denoting the number of test cases. 
+The first line of input contains an integer T denoting the number of test cases.
 Then T test cases follow. The first line of each test case contains an integer N denoting the length of the string.
 The second line of each test case contains the string consisting of only integer characters.
 
@@ -36,68 +36,84 @@ Output:
 
 */
 
+/*
+
+md[i,j] = min
+{
+1+md[i+1,j],
+md[i+2,k-1] + md[k+1,j], if s[k] == s[i]
+1 + md[i+2,j] if s[i] == s[i+1]
+}
+
+
+*/
+
+
 #include<iostream>
 #include<vector>
 #include<string>
 using namespace std;
 
-int LongestPalindromicSubtring(string& a, int& start, int& end)
+int MinimumDeletion(string& strText)
 {
-    int ret = 0;
-    int maxlen = 1;
-    int len = a.size();
-    vector< vector<int> >  LPS(len+1, vector<int>(len+1,0));
-    
-    start = end = 0;
-    //for length = 1 initialize
-    for(int i = 1; i<=len; ++i)
-    {
-        LPS[i][i] = 1;
-    }
-    for( int l = 2; l<=len; ++l)
-    {
-        for(int i = 1; i<=len; ++i)
-        {
-            int j = i+l-1;
-            if(j>len)
-            {
-                break;
-            }
-            LPS[i][j] = 0;
-            if(a[i-1] == a[j-1]) // both endpoints are the same
-            {
-                if(l == 2)
-                {
-                    LPS[i][j] = 2;
-                }
-                else
-                {
-                    LPS[i][j] = 0;
-                    if(LPS[i+1][j-1])
-                    {
-                        LPS[i][j] = LPS[i+1][j-1] + 2;
-                    }
-                }
-                if(LPS[i][j]>maxlen)
-                {
-                    maxlen = LPS[i][j];
-                    start = i-1;
-                    end = j-1;
-                }
-            }            
-        }
-    }
+	int ret = 0;
+	int L = strText.length();
+	vector< vector<int> > md(L + 1, vector<int>(L + 1, 0));
 
-    return ret;
+	for (int i = 0; i<L; ++i)
+	{
+		md[i][i] = 1;
+	}
+
+	for (int l = 2; l <= L; ++l)
+	{
+		for (int i = 0; i <= L - l; ++i)
+		{
+			int j = i + l - 1;
+			md[i][j] = 1 + md[i + 1][j]; //delete s[i] alone
+			if (strText[i] == strText[i + 1] && md[i][j]>1 + md[i + 2][j]) // edge case.
+			{
+				md[i][j] = 1 + md[i + 2][j];
+			}
+
+			/*
+			loop over all right characters and suppose Kth char is same as ith character 
+			then choose minimum from current and two substring after ignoring ith and Kth char
+			*/
+
+			for (int k = i + 2; k<=j; ++k)
+			{
+				if (strText[i] == strText[k] && md[i][j]>(md[i + 1][k - 1] + md[k + 1][j]))
+				{
+					md[i][j] = md[i + 1][k - 1] + md[k + 1][j];
+				}
+			}
+		}
+	}
+
+	cout << md[0][L - 1] << endl;
+
+
+	return ret;
 }
 
 
 int main(int argc, char const *argv[])
 {
-    string x = "2553432";
-    int start = 0;
-    int end = 0;
-    LongestPalindromicSubtring(x,start,end);
-    cout<<(end-start+1)<<endl;
-    return 0;
+	string x = "2553432";
+	//MinimumDeletion(x);
+	int t = 0;
+	
+	cin>>t;
+
+	while (t--)
+	{
+		int N = 0;
+		cin>>N;
+		string x;
+		cin>>x;
+		MinimumDeletion(x);
+	}
+
+	return 0;
 }
