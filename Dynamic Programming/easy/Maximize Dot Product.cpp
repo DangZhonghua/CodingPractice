7dccfccb9354871f302b877ec900f01070d6e2f5
Richard.Dang <richard.dang@arcserve.com>
2019-01-10 19:01:44 +0800

    w

--- Dynamic Programming/easy/Maximize Dot Product.cpp
+++ Dynamic Programming/easy/Maximize Dot Product.cpp
@@ -2,12 +2,16 @@
 https://practice.geeksforgeeks.org/problems/maximize-dot-product/0/?ref=self
 Maximize Dot Product 
 
-Given a array A with N elements and array B with M elements. You have to insert (N-M) zeroes in between array B such that the dot product of array A and array B is maximum. 
+Given a array A with N elements and array B with M elements. You have to insert (N-M) zeroes in 
+between array B such that the dot product of array A and array B is maximum. 
 
 Definition of Dot Product : A.B = A[0]*B[0] + A[1]*B[1]+....A[N]*B[N].
 
 Input :
-The first line will contain T which denotes the number of test cases. The first line of each test case will contain integer N and M, denoting the length of array A and array B respectively. The second line of each test case will contain N integers denoting the elements of array A. The third line of each test case will contain M integers denoting the elements of array B.
+The first line will contain T which denotes the number of test cases. 
+The first line of each test case will contain integer N and M, denoting the length of array A and array B respectively. 
+The second line of each test case will contain N integers denoting the elements of array A. 
+The third line of each test case will contain M integers denoting the elements of array B.
  
 
 Output:
@@ -24,11 +28,8 @@ Example
 Input : 
 
 1
-
 3 1 
-
 1 2 3
-
 4
 
 Output :
