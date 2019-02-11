77153925ceb9b74044a01ceb71bd673d92563174
Richard.Dang <richard.dang@arcserve.com>
2019-02-11 17:22:08 +0800

    h

--- LeetCode/Hard/Minimum Window Substring.cpp
+++ LeetCode/Hard/Minimum Window Substring.cpp
@@ -30,134 +30,21 @@ If there is such window, you are guaranteed that there will always be only one u
 #include<utility>
 using namespace std;
 
-// class Solution {
-// public:
-// 	string minWindow(string s, string t)
-// 	{
-// 		struct chPos
-// 		{
-// 			vector<int> vp;
-// 			int count{ 0 };
-// 			int index{ 0 };
-// 		};
 
-// 		unordered_map<char, chPos > dict;
-// 		bool bFind = true;
-// 		int  ws = -1;
-// 		int  we = -1;
-// 		int  wmin = -1;
-// 		int  fws = -1;
-// 		int  fwe = -1;
-// 		string strw = "";
-
-// 		//Build map for char in S about their position.
-// 		for (int i = 0; i < s.length(); ++i)
-// 		{
-// 			if (dict.end() != dict.find(s[i]))
-// 			{
-// 				dict[s[i]].count += 1;
-// 				dict[s[i]].vp.push_back(i);
-// 			}
-// 			else
-// 			{
-// 				chPos a;
-// 				a.count += 1;
-// 				a.vp.push_back(i);
-// 				dict[s[i]] = a;
-// 			}
-// 		}
-
-// 		for (int i = 0; i < t.length(); ++i)
-// 		{
-// 			if (dict.end() == dict.find(t[i]))
-// 			{
-// 				bFind = false;
-// 				break;
-// 			}
-// 		}
-// 		if (!bFind)
-// 		{
-// 			return strw;
-// 		}
-
-// 		while (1)
-// 		{
-// 			int Lenp = t.length();
-// 			int start = 0;
-// 			chPos& fp = dict[t[0]];
-// 			if (fp.index >= fp.count)
-// 			{
-// 				break;
-// 			}
-// 			ws = fp.vp[fp.index++];
-// 			start = ws;
-// 			we = -1;
-// 			for (int i = 1; i < Lenp; ++i)
-// 			{
-// 				bool bNext = false;
-// 				chPos& fnp = dict[t[i]];
-// 				for (int next = fnp.index; next < fnp.count; ++next)
-// 				{
-// 					if (start < fnp.vp[next])
-// 					{
-// 						start = fnp.vp[next];
-// 						fnp.index = next;
-// 						bNext = true;
-// 						break;
-// 					}
-// 				}
-// 				if (bNext)
-// 				{
-// 					we = start;
-// 				}
-// 				else
-// 				{
-// 					break;
-// 				}
-// 			}
-// 			if (-1 == we)
-// 			{
-// 				break;
-// 			}
-// 			else
-// 			{
-// 				if (-1 == wmin)
-// 				{
-// 					wmin = we - ws + 1;
-// 					fwe = we;
-// 					fws = ws;
-// 				}
-// 				else
-// 				{
-// 					if (wmin > (we - ws + 1))
-// 					{
-// 						wmin = (we - ws + 1);
-// 						fwe = we;
-// 						fws = ws;
-// 					}
-// 				}
-// 			}
-// 		}
-		
-// 		if (fwe > 0)
-// 		{
-// 			strw = s.substr(fws, wmin);
-// 		}
-// 		return strw;
-// 	}
-// };
-
-
-class Solution {
-public:
-	string minWindow(string s, string t)
+class Solution 
+{
+private:
+	struct wdata
 	{
-		struct wdata
-		{
 			bool bCheck{false};
 			int  tc{0};
 			int  sc{0};
-		};
+	};
+
+public:
+	string minWindow(string s, string t)
+	{
+
 		unordered_map<int,wdata > dict;
 		for(int i = 0; i<t.length(); ++i)
 		{
@@ -176,10 +63,9 @@ public:
 
 int main(int argc, char const *argv[])
 {
+	Solution sol;
 	string s = "cabwefgewcwaefgcf";
 	string t = "cae";
-	Solution sol;
-
 	s = "ADOBECODEBANC";
 	s = "BANC";
 
