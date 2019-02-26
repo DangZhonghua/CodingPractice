20fe533a9f255a91b3db94d4790197b8888ab462
DangZhonghua <forseekme@163.com>
2019-02-26 21:44:49 +0800

    w

---
+++ LeetCode/Hard/Binary Tree Cameras.cpp
@@ -0,0 +1,27 @@
+/*
+https://leetcode-cn.com/problems/binary-tree-cameras/
+Binary Tree Cameras
+
+Given a binary tree, we install cameras on the nodes of the tree. 
+
+Each camera at a node can monitor its parent, itself, and its immediate children.
+
+Calculate the minimum number of cameras needed to monitor all nodes of the tree.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minCameraCover(TreeNode* root) {
+        
+    }
+};
--- LeetCode/MedimumLevel/Partition to K Equal Sum Subsets.cpp
+++ LeetCode/MedimumLevel/Partition to K Equal Sum Subsets.cpp
@@ -17,85 +17,126 @@ Explanation: It's possible to divide it into 4 subsets (5), (1, 4), (2,3), (2,3)
 
 Note:
 
-1 <= k <= len(nums) <= 16.
-0 < nums[i] < 10000.
+	1 <= k <= len(nums) <= 16.
+	0 < nums[i] < 10000.
 
 */
 
 
 #include<iostream>
 #include<vector>
-#include <algorithm>
+#include<algorithm>
 using namespace std;
 
 class Solution
 {
 public:
-	bool canPartitionKSubsets(vector<int>& nums, int K)
+	bool canPartitionKSubsetsDP(vector<int>& nums, int K)
 	{
 		std::sort(nums.begin(), nums.end());
 		bool bPartition = true;
 		int S = 0;
-		for (int e : nums)
+		bPartition = CalAverage(nums, S, K);
+		if (!bPartition)
 		{
-			S += e;
-		}
-		if (S%K)
-		{
-			return false;
+			return bPartition;
 		}
 
-		S /= K;
 		int N = nums.size();
-		vector< vector<int> >  vpart(K, vector<int>(S + 1, -1));
-		vector< bool > vBits(N + 1, false);
+		vector< vector< vector<int> > > dp(K, vector< vector<int > >(N + 1, vector<int>(S + 1, -1)));
+		vector<bool> vBits(N + 1, false);
 
 		for (int k = 0; k < K; ++k)
 		{
-			vpart[k][0] = 0;
+			for (int i = 0; i <= N; ++i)
+			{
+				dp[k][i][0] = 0;
+			}
 		}
 
-		for (int k = 0; k<K; ++k)
+		for (int k = 0; k < K; ++k)
 		{
-			vector<int> vcBits(N + 1, false);
+			dp[k][0][0] = 0;
+
 			for (int i = 1; i <= N; ++i)
 			{
-				if (vBits[i] || vcBits[i])
-				{
-					continue;
-				}
 				for (int s = 1; s <= S; ++s)
 				{
-					if (!vcBits[i]  && s >= nums[i - 1] && -1 != vpart[k][s - nums[i - 1]])
+
+					if (!vBits[i] && s >= nums[i - 1] && -1 != dp[k][i - 1][s - nums[i - 1]])
 					{
-						vpart[k][s] = i;
-						vcBits[i] = true;
+						dp[k][i][s] = i;
+					}
+					else
+					{
+						dp[k][i][s] = dp[k][i - 1][s];
 					}
 				}
 			}
-
-			if (-1 == vpart[k][S])
+			if (-1 == dp[k][N][S])
 			{
 				bPartition = false;
 				break;
 			}
-
-			int s = S;
-			int j = 0;
-			while (s)
+			
+			int m = S;
+			int j = dp[k][N][S];
+			while (m && j)
 			{
-				j = vpart[k][s];
-				s -= nums[j - 1];
-				vBits[j] = true;
 				cout << nums[j - 1] << " ";
+				vBits[j] = true;
+				m -= nums[j - 1];
+				j = dp[k][j-1][m];
 			}
 			cout << endl;
 		}
 
 		return bPartition;
-
 	}
+	bool canPartitionKSubsets(vector<int>& nums, int K)
+	{
+		
+	}
+
+	bool PartitionKSubsets(vector<int>& nums, vector<bool>& vBits, vector< vector<int> > vsets, int k, vector<int>& vsum)
+	{
+		if (k = m_K)
+		{
+			return true;
+		}
+		for (int i = 0; i < nums.size(); ++i)
+		{
+			if (!vBits[i])
+			{
+				vsum[k] += nums[i];
+				if (vsum[k] == m_S)
+				{
+					vBits[i] = true;	
+				}
+			}
+		}
+	}
+
+private:
+	bool CalAverage(vector<int>& nums, int& S, int K)
+	{
+		int sum = 0;
+		for (int e : nums)
+		{
+			sum += e;
+		}
+		if (sum % K)
+		{
+			return false;
+		}
+		S = sum / K;
+		return true;
+	}
+private:
+	int m_S{ 0 };
+	int m_K{ 0 };
 };
+
 
 int main()
 {
@@ -110,21 +151,20 @@ int main()
 
 	vector<int> nums2{ 4, 3, 2, 3, 5, 2, 1 };
 	k = 4;
+	//cout << sol.canPartitionKSubsets(nums2, k) << endl;
 
 
+	vector<int> nums3{ 2, 2, 2, 2, 3, 4, 5 };
+	k = 4;
 
-	cout << sol.canPartitionKSubsets(nums2, k) << endl;
+
+	//cout << sol.canPartitionKSubsets(nums3, k) << endl;
 
 
-	vector<int> nums3{ 4, 5, 3, 2, 5, 5, 5, 1, 5, 5, 5, 5, 3, 5, 5, 2 };
+	vector<int> nums4{ 10, 10, 10, 7, 7, 7, 7, 7, 7, 6, 6, 6 };
+	k = 3;
 
-	//{ 4,  3, 2, 1, 3, 2 }
-	k = 13;
-
-	//vector<int> nums4{ 7, 2, 2, 2, 2, 2, 2, 2, 3 };
-	//k = 3;
-
-	//cout << sol.canPartitionKSubsets(nums4, k) << endl;
+	cout << sol.canPartitionKSubsets(nums4, k) << endl;
 
 	return 0;
 }
