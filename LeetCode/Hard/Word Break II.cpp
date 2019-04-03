fcf25e864b8d5ed2655ba3a7d0f40e662932705b
Richard.Dang <richard.dang@arcserve.com>
2019-04-03 14:17:28 +0800

    w

--- LeetCode/Hard/Word Break II.cpp
+++ LeetCode/Hard/Word Break II.cpp
@@ -41,10 +41,195 @@ Output:
 #include<vector>
 using namespace std;
 
-class Solution {
+
+class Trie 
+{
+    struct  Node
+    {
+        char  szLetter[26];
+        Node* szNext[26];
+        bool  szEnd[26];
+
+        Node()
+        {
+            for(int i = 0; i<26; ++i)
+            {
+                szLetter[i] = 0;
+                szNext[i]   = nullptr;
+                szEnd[i]    = false;
+            }
+        }
+    };
+
+public:
+    /** Initialize your data structure here. */
+    Trie() 
+    {
+        m_root = new Node();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(const string& word) 
+    {
+        int   N = word.length();
+        int   i = 0;
+        Node* p = m_root;
+
+        while(i< N)
+        {
+            int index = word[i]-'a';
+            p->szLetter[index] = 1;
+            if(i+1 == N)
+            {
+                p->szEnd[index] = true;
+                break;
+            }
+            if(nullptr == p->szNext[index])
+            {
+                p->szNext[index] = new Node();       
+            }
+            p = p->szNext[index];
+            ++i;       
+        }
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(const string& word) 
+    {
+        int N = word.length();
+        int i = 0;
+        bool bExist = true;
+        Node* p = m_root;
+
+        while(i<N)
+        {
+            int index = word[i]-'a';
+            if( 0 == p->szLetter[index])
+            {
+                bExist = false;
+                break;
+            }
+            if( i+1 == N)
+            {
+                if(!p->szEnd[index])
+                {
+                    bExist = false;
+                }
+                break;
+            }
+            p = p->szNext[index];
+            if(nullptr == p)
+            {
+                bExist = false;
+                break;
+            }
+            ++i;
+        }
+        return bExist;    
+    }
+
+    bool search(const char* word, int N) 
+    {
+        int i = 0;
+        bool bExist = true;
+        Node* p = m_root;
+
+        while(i<N)
+        {
+            int index = word[i]-'a';
+            if( 0 == p->szLetter[index])
+            {
+                bExist = false;
+                break;
+            }
+            if( i+1 == N)
+            {
+                if(!p->szEnd[index])
+                {
+                    bExist = false;
+                }
+                break;
+            }
+            p = p->szNext[index];
+            if(nullptr == p)
+            {
+                bExist = false;
+                break;
+            }
+            ++i;
+        }
+        return bExist;    
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(const string& prefix) 
+    {
+        int N = prefix.length();
+        int i = 0;
+        bool bExist = true;
+        Node* p = m_root;
+
+        while(i<N)
+        {
+            int index = prefix[i]-'a';
+            if( 0 == p->szLetter[index])
+            {
+                bExist = false;
+                break;
+            }
+            if( i+1 == N)
+            {
+                break;
+            }
+            p = p->szNext[index];
+            if(nullptr == p)
+            {
+                bExist = false;
+                break;
+            }
+            ++i;
+        }
+        return bExist;   
+    }
+
+private:
+   Node* m_root{nullptr};
+};
+
+
+
+
+class Solution 
+{
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) 
     {
+        int N = s.length();
+        vector< vector<bool> >  dp(N+1, vector<bool>(N+1,false));
         
     }
+
+
+private:
+    void BuildDictinoary(vector<string>& wordDict)
+    {
+        m_pDict = new Trie();
+        for(auto s: wordDict)
+        {
+            m_pDict->insert(s);
+        }
+    }
+
+    bool SearchWord(const char* szWord, int Len)
+    {
+        bool bExist = true;
+
+        bExist = m_pDict->search(szWord,Len);
+
+        return bExist;
+    }
+
+private:
+    Trie* m_pDict{nullptr};
+
 };
--- LeetCode/MedimumLevel/Word Break.cpp
+++ LeetCode/MedimumLevel/Word Break.cpp
@@ -32,44 +32,219 @@ using namespace std;
 
 DP[i] = DP[k] + DP[k+1,j]
 DP[m,n] whether the s[m,n] in the dictinoary
+use Trie tree as dictinoary.
 
 */
 
 
+class Trie 
+{
+    struct  Node
+    {
+        char  szLetter[26];
+        Node* szNext[26];
+        bool  szEnd[26];
+
+        Node()
+        {
+            for(int i = 0; i<26; ++i)
+            {
+                szLetter[i] = 0;
+                szNext[i]   = nullptr;
+                szEnd[i]    = false;
+            }
+        }
+    };
+
+public:
+    /** Initialize your data structure here. */
+    Trie() 
+    {
+        m_root = new Node();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(const string& word) 
+    {
+        int   N = word.length();
+        int   i = 0;
+        Node* p = m_root;
+
+        while(i< N)
+        {
+            int index = word[i]-'a';
+            p->szLetter[index] = 1;
+            if(i+1 == N)
+            {
+                p->szEnd[index] = true;
+                break;
+            }
+            if(nullptr == p->szNext[index])
+            {
+                p->szNext[index] = new Node();       
+            }
+            p = p->szNext[index];
+            ++i;       
+        }
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(const string& word) 
+    {
+        int N = word.length();
+        int i = 0;
+        bool bExist = true;
+        Node* p = m_root;
+
+        while(i<N)
+        {
+            int index = word[i]-'a';
+            if( 0 == p->szLetter[index])
+            {
+                bExist = false;
+                break;
+            }
+            if( i+1 == N)
+            {
+                if(!p->szEnd[index])
+                {
+                    bExist = false;
+                }
+                break;
+            }
+            p = p->szNext[index];
+            if(nullptr == p)
+            {
+                bExist = false;
+                break;
+            }
+            ++i;
+        }
+        return bExist;    
+    }
+
+    bool search(const char* word, int N) 
+    {
+        int i = 0;
+        bool bExist = true;
+        Node* p = m_root;
+
+        while(i<N)
+        {
+            int index = word[i]-'a';
+            if( 0 == p->szLetter[index])
+            {
+                bExist = false;
+                break;
+            }
+            if( i+1 == N)
+            {
+                if(!p->szEnd[index])
+                {
+                    bExist = false;
+                }
+                break;
+            }
+            p = p->szNext[index];
+            if(nullptr == p)
+            {
+                bExist = false;
+                break;
+            }
+            ++i;
+        }
+        return bExist;    
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(const string& prefix) 
+    {
+        int N = prefix.length();
+        int i = 0;
+        bool bExist = true;
+        Node* p = m_root;
+
+        while(i<N)
+        {
+            int index = prefix[i]-'a';
+            if( 0 == p->szLetter[index])
+            {
+                bExist = false;
+                break;
+            }
+            if( i+1 == N)
+            {
+                break;
+            }
+            p = p->szNext[index];
+            if(nullptr == p)
+            {
+                bExist = false;
+                break;
+            }
+            ++i;
+        }
+        return bExist;   
+    }
+
+private:
+   Node* m_root{nullptr};
+};
+
 
 class Solution 
 {
-    struct TreeItem
-    {
-        int c;
-        struct TreeItem* next;
-    };
 public:
     bool wordBreak(string s, vector<string>& wordDict) 
     {
-        vector<bool> sb(s.length(),false);
-        for(int i = 0; i<s.length();++i)
+        int N = s.length();
+        vector<bool>  vFlag(N+2, false);
+        char* szText = new char[N+2];
+        //strcpy_s(szText,N+1,s.c_str());
+        strcpy(szText+1,s.c_str());
+        
+        vFlag[0] = true;
+        BuildDictinoary(wordDict);
+
+        for(int i = 1; i<=N; ++i )
         {
-            for(int j = 0;j<s.length(); ++j)
-               
-        }
-
-
-
-    }
-private:
-    bool SearchDict(vector<string>& wordDict, const char* szWord)
-    {
-        for(string& str:wordDict)
-        {
-            if(!strcmp(str.c_str(),szWord))
+            for(int j = 0; j<i; ++j)
             {
-                return true;
+                if(vFlag[j]) //Find a position which is the next start matching point
+                {
+                    char c = szText[i+1];
+                    szText[i+1] = '\0';
+                    if(SearchWord(szText+j+1, i-j))
+                    {
+                        vFlag[i] = true;
+                        szText[i+1] = c;
+                        break;
+                    }
+                     szText[i+1] = c;
+                }
             }
         }
-        return false;
-    } 
+        return vFlag[N];
+    }
+private:
+    void BuildDictinoary(vector<string>& wordDict)
+    {
+        m_pDict = new Trie();
+        for(auto s: wordDict)
+        {
+            m_pDict->insert(s);
+        }
+    }
+
+    bool SearchWord(const char* szWord, int Len)
+    {
+        bool bExist = true;
+
+        bExist = m_pDict->search(szWord,Len);
+
+        return bExist;
+    }
 
 private:
-    
+    Trie* m_pDict{nullptr};
 };
