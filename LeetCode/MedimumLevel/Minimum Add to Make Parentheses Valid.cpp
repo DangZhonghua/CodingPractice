c1653ccc0a502863382b203b3ea6fe30b48c1c6e
DangZhonghua <forseekme@163.com>
2019-04-25 21:08:16 +0800

    w

--- LeetCode/MedimumLevel/Minimum Add to Make Parentheses Valid.cpp
+++ LeetCode/MedimumLevel/Minimum Add to Make Parentheses Valid.cpp
@@ -8,7 +8,7 @@ It is the empty string, or
 It can be written as AB (A concatenated with B), where A and B are valid strings, or
 It can be written as (A), where A is a valid string.
 Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.
- 
+ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 Example 1:
 Input: "())"
 Output: 1
