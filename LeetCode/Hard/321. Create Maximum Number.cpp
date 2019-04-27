c42c02e926578202ecaf3d41bcb8374f5b1007ea
DangZhonghua <forseekme@163.com>
2019-04-27 21:54:03 +0800

    w

--- LeetCode/Hard/321. Create Maximum Number.cpp
+++ LeetCode/Hard/321. Create Maximum Number.cpp
@@ -1,8 +1,14 @@
 /*
+
 https://leetcode-cn.com/problems/create-maximum-number/
+http://www.cnblogs.com/grandyang/p/5136749.html
+
 321. Create Maximum Number
 
-Given two arrays of length m and n with digits 0-9 representing two numbers. Create the maximum number of length k <= m + n from digits of the two. The relative order of the digits from the same array must be preserved. Return an array of the k digits.
+Given two arrays of length m and n with digits 0-9 representing two numbers. 
+Create the maximum number of length k <= m + n from digits of the two. 
+The relative order of the digits from the same array must be preserved. 
+Return an array of the k digits.
 Note: You should try to optimize your time and space complexity.
 Example 1:
 Input:
---
+++ LeetCode/Hard/Remove Duplicate Letters.cpp
@@ -0,0 +1,20 @@
+/*
+https://leetcode-cn.com/problems/remove-duplicate-letters/
+Remove Duplicate Letters
+Given a string which contains only lowercase letters, remove duplicate letters so that 
+every letter appear once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+Example 1:
+Input: "bcabc"
+Output: "abc"
+Example 2:
+Input: "cbacdcbc"
+Output: "acdb"
+
+*/
+
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        
+    }
+};
