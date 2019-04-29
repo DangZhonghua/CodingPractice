e7b2c1abb30be77d651d71486f0c5c403ae18a7e
DangZhonghua <forseekme@163.com>
2019-04-29 17:08:26 +0800

    w

--- LeetCode/Hard/321. Create Maximum Number.cpp
+++ LeetCode/Hard/321. Create Maximum Number.cpp
@@ -1,77 +1,49 @@
-/*
-
-https://leetcode-cn.com/problems/create-maximum-number/
-http://www.cnblogs.com/grandyang/p/5136749.html
-
-321. Create Maximum Number
-
-Given two arrays of length m and n with digits 0-9 representing two numbers. 
-Create the maximum number of length k <= m + n from digits of the two. 
-The relative order of the digits from the same array must be preserved. 
-Return an array of the k digits.
-Note: You should try to optimize your time and space complexity.
-Example 1:
-Input:
-nums1 = [3, 4, 6, 5]
-nums2 = [9, 1, 2, 5, 8, 3]
-k = 5
-Output:
-[9, 8, 6, 5, 3]
-Example 2:
-Input:
-nums1 = [6, 7]
-nums2 = [6, 0, 4]
-k = 5
-Output:
-[6, 7, 6, 0, 4]
-Example 3:
-Input:
-nums1 = [3, 9]
-nums2 = [8, 9]
-k = 3
-Output:
-[9, 8, 9]
-
-*/
-
-
 #include<vector>
 #include<stack>
 #include<iostream>
 using namespace std;
 
 
-class Solution 
+class Solution
 {
 public:
-    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) 
-    {
-        vector<int> vselect1;
-        vector<int> vselect2;
-        vector<int> vr;
+	vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k)
+	{
+		vector<int> vselect1;
+		vector<int> vselect2;
+		vector<int> vr;
 
-        int N1 = nums1.size();
-        int N2 = nums2.size();
-        
+		int N1 = nums1.size();
+		int N2 = nums2.size();
 
-        for(int s = 0; s<=N1 && s<=k;  ++s )
-        {
-            vselect1.clear();
-            vselect2.clear();
-            
-            if( (k-s) <= N2)
-            {
-                SelectMaximumNumber(nums1,vselect1,s);
-                SelectMaximumNumber(nums2,vselect2,k-s);
-                
-            }
-        }
 
-    }
+		for (int s = 0; s <= N1 && s <= k; ++s)
+		{
+			vselect1.clear();
+			vselect2.clear();
+
+			if ((k - s) <= N2)
+			{
+				SelectMaximumNumber(nums1, vselect1, s);
+				SelectMaximumNumber(nums2, vselect2, k - s);
+				vector<int> vc;
+				CombineMaximumNumber(vselect1, vselect2, vc);
+				AcquireMaxinumNumber(vc, vr);
+			}
+		}
+
+		for (auto e : vr)
+		{
+			cout << e << " ";
+		}
+		cout << endl;
+
+		return vr;
+	}
 
 private:
 
-    void SelectMaximumNumber(vector<int>& nums, vector<int>& vselect, int k)
+	void SelectMaximumNumber(vector<int> & nums, vector<int> & vselect, int k)
 	{
 		int N = nums.size();
 		int nDrop = N - k;
@@ -92,19 +64,78 @@ private:
 			}
 			vselect.push_back(nums[i]);
 		}
+		
+		while (!vselect.empty() && nDrop > 0)
+		{
+			vselect.pop_back();
+			--nDrop;
+		}
 
 		return;
 	}
 
-    void CombineMaximumNumber(vector<int>& vselect1, vector<int>& vselect2, vector<int>& vc)
-    {
-        int i = 0;
-        int j = 0;
-        while( i<vselect1.size() && j<vselect2.size() )
-        {
-            if(vselect1)
-        }
-    }
+	void CombineMaximumNumber(vector<int> & vselect1, vector<int> & vselect2, vector<int> & vc)
+	{
+		int i = 0;
+		int j = 0;
+		int si = 0;
+		int sj = 0;
+
+		while (i < vselect1.size() && j < vselect2.size())
+		{
+			if (vselect1[i] == vselect2[j])
+			{
+				++i;
+				++j;
+			}
+			else if (vselect1[i] > vselect2[j])
+			{
+				for (; si <= i; ++si)
+				{
+					vc.push_back(vselect1[si]);
+				}
+				++i;
+			}
+			else
+			{
+				for (; sj <= j; ++sj)
+				{
+					vc.push_back(vselect2[sj]);
+				}
+				++j;
+			}
+		}
+
+		while (si < vselect1.size())
+		{
+			vc.push_back(vselect1[si++]);
+		}
+		while (sj < vselect2.size())
+		{
+			vc.push_back(vselect2[sj++]);
+		}
+	}
+
+	void AcquireMaxinumNumber(vector<int> & vc, vector<int> & vr)
+	{
+		if (vr.empty())
+		{
+			vr = vc;
+		}
+		//like string compare
+		int i = 0;
+
+		for (int i = 0; i < vc.size(); ++i)
+		{
+			if (vc[i] > vr[i])
+			{
+				vr = vc;
+				break;
+			}
+			else if (vc[i] < vr[i])
+			{
+				break;
+			}
+		}
+	}
 };
-
-
