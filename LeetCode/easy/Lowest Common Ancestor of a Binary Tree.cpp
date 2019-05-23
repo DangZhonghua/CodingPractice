7f6e6220f20a39baf17709d3578bea4f5c30134b
DangZhonghua <forseekme@163.com>
2019-05-23 21:52:54 +0800

    w'

--- LeetCode/easy/Lowest Common Ancestor of a Binary Tree.cpp
+++ LeetCode/easy/Lowest Common Ancestor of a Binary Tree.cpp
@@ -3,7 +3,9 @@ https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
 Lowest Common Ancestor of a Binary Tree
 
 Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
-According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+According to the definition of LCA on Wikipedia: 
+“The lowest common ancestor is defined between two nodes p and q as the lowest node in T 
+that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
 
 Example 1:
@@ -29,9 +31,70 @@ p and q are different and both values will exist in the binary tree.
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ struct TreeNode 
+ {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+
+
+/*
+
+use pre-order to find the target element path.
+
+*/
+
+class Solution 
+{
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        vector<TreeNode*> vpathp;
+        vector<TreeNode*> vpathq;
         
+        if(root)
+        {
+            preOrderPath(root, p, vpathp);
+            preOrderPath(root, q, vpathq);
+        }
+        if( vpathp.size() && vpathq.size())
+        {
+            
+        }
+    }
+
+private:
+    bool preOrderPath(TreeNode* node, TreeNode* p, vector<TreeNode*>& vp)
+    {
+        vp.push_back(node);
+        if(p == node)
+        {
+            return true;
+        }
+        if(node->left)
+        {
+            if( preOrderPath(node->left,p,vp) )
+            {
+                return true;
+            }
+        }
+        
+        if(node->right)
+        {
+            if( preOrderPath(node->right,p,vp) )
+            {
+                return true;
+            }
+        }
+        return false;
     }
 };
